5daa7c0fcade7a809affbafa40fae2c43176964e
Ian Denhardt <ian@zenhack.net>
2016-10-19 22:59:00 -0400

    Start working on reorganizing

---
+++ Data/CapNProto/Untyped.hs
@@ -0,0 +1,51 @@
+module Data.CapNProto.Untyped where
+
+import Data.Word
+import Data.Int
+import Data.Vector ((!))
+import qualified Data.Vector as BV
+import qualified Data.Vector.Unboxed as UV
+
+import Control.Monad.Catch (MonadThrow, throwM)
+import Control.Exception (ArrayException(IndexOutOfBounds))
+
+type Segment = UV.Vector Word64
+type Message = BV.Vector Segment
+
+data Address = Address
+    !Int -- ^ Segment number
+    !Int -- ^ Word index in segment
+    deriving(Show)
+
+data Pointer
+    = Struct !Int32 !Word16 !Word16
+    | Capability !Word32
+    | List !Int32 !ElementSize
+    | Far LandingSize Int32 Word32
+    deriving(Show)
+
+data PointerDest
+    = WordDest Address
+    | CapDest Word32
+    deriving(Show)
+
+data LandingSize = OneWord | TwoWords deriving(Show, Enum)
+
+data ElementSize
+    = Bit
+    | Byte
+    | TwoBytes
+    | FourBytes
+    | EightBytesVal
+    | EightBytesPtr
+    | Composite
+    deriving(Show, Enum)
+
+
+followPtr :: (MonadThrow m) => Message -> Address -> Pointer -> m PointerDest
+followPtr msg addr@(Address segnum wordidx) _
+    | segnum < 0
+      || wordidx < 0
+      || segnum >= BV.length msg
+      || wordidx >= UV.length (msg ! segnum)
+      = throwM $ IndexOutOfBounds (show addr)
