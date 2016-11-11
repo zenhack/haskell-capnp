434654e99d0091d46dc405fc355eb297ce09496f
Ian Denhardt <ian@zenhack.net>
2016-11-11 02:48:31 -0500

    Work on a module for stream serialization
    Built on top of the quota package I've written; this should be less
    error-prone than before.

---
+++ Data/CapNProto/Stream.hs
@@ -0,0 +1,47 @@
+{-|
+Module : Data.CapNProto.Stream
+Description : Serialize CapNProto messages over streams.
+
+This module implements the encoding described at:
+
+https://capnproto.org/encoding.html#serialization-over-a-stream
+
+-}
+module Data.CapNProto.Stream
+    ( getMessage
+    , putMessage
+    ) where
+
+import Data.CapNProto.Untyped (Segment, Message)
+import Data.Bytes.Get (MonadGet, getWord32le, getWord64le)
+import Data.Bytes.Put (MonadPut, putWord32le, putWord64le)
+import Control.Monad.Quota (MonadQuota(invoice))
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector as BV
+
+-- | Read in a Cap'n Proto message.
+--
+-- The size of the message will be invoiced to the 'MonadQuota' instance,
+-- so unmarshalling may stop if if the cursor's quota is exceeded.
+getMessage :: (MonadGet m, MonadQuota m) => m Message
+getMessage = do
+    numSegments <- get32
+    segmentSizes <- UV.replicateM numSegments get32
+    -- pad up to the next word boundary:
+    when ((1 + UV.length segmentSizes) `mod` 2 /= 0) get32
+    UV.mapM getSegment segmentSizes
+  where
+    getLimited size getter = invoice size >> getter
+    get32 = getLimited 4 getWord32le
+    get64 = getLimited 8 getWord64le
+    getSegment size = UV.replicateM size get64
+
+-- | Write out a Cap'n Proto message.
+putMessage :: (MonadPut m) => Message -> m ()
+putMessage msg = do
+    putWord32le (BV.length msg)
+    BV.mapM_ (putWord32le . UV.length) msg
+    when ((1 + BV.length msg) `mod` 2 /= 0) (putWord32le 0)
+    BV.mapM_ putSegment msg
+  where
+    putSegment seg = UV.mapM_ putWord64le seg
