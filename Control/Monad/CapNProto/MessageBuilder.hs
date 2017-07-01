46f77fbe45e01ab8ad28c86d4a971fe77541b750
Ian Denhardt <ian@zenhack.net>
2017-07-01 17:51:58 -0400

    Swap out vector for MutBlob in MessageBuilder.
    This resolves #2.
    
    Future work:
    
    * fully remove the dependency on vector. We're still using it to index
      segments in the message, but we could use stuff from primitive instead
      without any real trouble.
    * More instances of Blob/MutBlob.

--- Control/Monad/CapNProto/MessageBuilder.hs
+++ Control/Monad/CapNProto/MessageBuilder.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards, MultiParamTypeClasses #-}
 module Control.Monad.CapNProto.MessageBuilder
     ( BuilderT
     , runBuilderT
@@ -8,31 +8,37 @@ module Control.Monad.CapNProto.MessageBuilder
 import Control.Monad (when)
 import Control.Monad.Primitive (PrimMonad, PrimState)
 import Control.Monad.State (StateT, get, modify, evalStateT)
+import Control.Monad.Trans.Class (MonadTrans(lift))
 import Data.Word
-import qualified Data.Vector.Generic.Mutable as V
 
-data BuilderState s v = BuilderState
+import qualified Data.CapNProto.Blob as B
+
+class B.Blob m b => MutBlob m b where
+    write :: b -> Int -> Word64 -> m ()
+    grow :: b -> Int -> m b
+    new :: Int -> m b
+
+data BuilderState s b = BuilderState
     { nextAlloc :: Int
-    , segment :: v s Word64
+    , segment :: b
     }
 
+newtype BuilderT s b m a = BuilderT (StateT (BuilderState s b) m a)
 
-newtype BuilderT s v m a = BuilderT (StateT (BuilderState s v) m a)
-
-runBuilderT :: (PrimMonad m, V.MVector v Word64) =>
-    BuilderT (PrimState m) v m a -> m a
+runBuilderT :: (PrimMonad m, MutBlob m b) => BuilderT (PrimState m) b m a -> m a
 runBuilderT (BuilderT st) = do
-    segment <- V.new 0
+    segment <- new 0
     evalStateT st BuilderState { nextAlloc = 0
                                , segment = segment
                                }
 
 
-alloc :: (PrimMonad m, V.MVector v Word64) => Int -> BuilderT (PrimState m) v m Int
+alloc :: (PrimMonad m, MutBlob m b) => Int -> BuilderT (PrimState m) b m Int
 alloc size = BuilderT $ do
     BuilderState{..} <- get
-    when (V.length segment - nextAlloc < size) $ do
-        segment' <- V.grow segment (nextAlloc + 1)
+    blobLen <- lift $ B.length segment
+    when (blobLen - nextAlloc < size) $ do
+        segment' <- lift $ grow segment (nextAlloc + 1)
         modify $ \bs -> bs { segment = segment' }
     modify $ \bs -> bs { nextAlloc = nextAlloc + size }
     return nextAlloc
