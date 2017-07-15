3c9812852274db412f93a9b51dd2730c82093409
Ian Denhardt <ian@zenhack.net>
2017-07-14 20:56:34 -0400

    Remove a bunch of unused imports in Main

--- Main.hs
+++ Main.hs
@@ -1,15 +1,7 @@
 module Main where
 
-import Control.Monad.Catch (Exception, MonadThrow, throwM)
-import Control.Monad.Quota (QuotaT(..), Quota(..))
-import Control.Monad.State (MonadState, put, get, StateT(..))
-import Data.Bits (shiftL, (.|.))
 import qualified Data.ByteString as BS
 import qualified Data.CapNProto.Message as M
-import Data.Word (Word8, Word32, Word64)
-import qualified Data.Vector as B
-import qualified Data.Vector.Unboxed as U
-
 
 main :: IO ()
 main = do
