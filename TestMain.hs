0a9a3640a91c41dbf39ef8788cb44406518ed2e7
Ian Denhardt <ian@zenhack.net>
2017-08-13 14:41:39 -0400

    Write a (failing) test for MessageBuilder

--- TestMain.hs
+++ TestMain.hs
@@ -5,9 +5,11 @@ import Test.Framework (defaultMain)
 import Tests.Data.CapNProto.Bits (bitsTests)
 import Tests.Data.CapNProto.Pointer (ptrTests)
 import Tests.Data.CapNProto.Untyped (untypedTests)
+import Tests.Control.Monad.CapNProto.MessageBuilder (buildTests)
 
 main :: IO ()
 main = defaultMain [ bitsTests
                    , ptrTests
                    , untypedTests
+                   , buildTests
                    ]
--- Tests/Control/Monad/CapNProto/MessageBuilder.hs
+++ Tests/Control/Monad/CapNProto/MessageBuilder.hs
@@ -1,1 +1,30 @@
-module Tests.Control.Monad.CapNProto.MessageBuilder where
+{-# LANGUAGE RecordWildCards, TypeFamilies #-}
+module Tests.Control.Monad.CapNProto.MessageBuilder (buildTests) where
+
+import Control.Monad.CapNProto.MessageBuilder
+import Data.CapNProto.Bits (WordCount(..))
+import Data.CapNProto.Blob (BlobSlice(..))
+
+import Control.Monad.Primitive (RealWorld)
+import Data.Primitive.ByteArray (readByteArray)
+import Data.Word
+
+import Test.Framework (testGroup)
+import Test.HUnit (assertEqual, Test(TestList, TestCase))
+import Test.Framework.Providers.HUnit (hUnitTestToTests)
+
+
+buildTest :: BuilderT p RealWorld IO () -> [Word8] -> Test
+buildTest builder expected =  TestCase $ do
+    (BlobSlice{..}, ()) <- runBuilderT builder
+    let WordCount off = offset
+    let WordCount len = sliceLen
+    actual <- mapM (readByteArray blob) [off..off+len]
+    assertEqual "" expected actual
+
+
+buildTests = testGroup "build tests" $ hUnitTestToTests $ TestList $ map (uncurry buildTest)
+    [ ( return ()
+      , []
+      )
+    ]
