78669d27cd07d9dc15c7b3cdfac0e7daf5494c05
Ian Denhardt <ian@zenhack.net>
2017-08-13 17:59:37 -0400

    Tests.Util: implement capnpDecode.
    This necessarily involved factoring out common logic from capnpEncode.

--- Tests/Util.hs
+++ Tests/Util.hs
@@ -2,16 +2,13 @@
 module Tests.Util where
 
 import Control.Concurrent (forkIO)
-import Control.Monad.Trans.Resource (runResourceT, allocate)
+import Control.Monad.Trans.Resource (ResourceT, runResourceT, allocate)
 import Control.Monad.Trans (lift)
 import GHC.IO.Handle (hSetBinaryMode)
 import qualified Data.ByteString as BS
 import System.IO
 import System.Process
 import System.Directory (removeFile)
-
-import Data.CapNProto.Blob (BlobSlice)
-import qualified Data.CapNProto.Message as M
 
 import Test.Framework (testGroup, Test)
 import qualified Test.HUnit as H
@@ -27,36 +24,57 @@ data MsgMetaData = MsgMetaData
 -- | @capnpEncode msg meta@ runs @capnp encode@ on the message, providing
 -- the needed metadata and returning the output
 capnpEncode :: String -> MsgMetaData -> IO BS.ByteString
-capnpEncode msgValue MsgMetaData{..} = runResourceT $ do
-    (_, schemaPath) <- allocate
-        (writeTempFile "schema.capnp" msgSchema)
-        removeFile
-    runCapnp schemaPath
+capnpEncode msgValue meta@MsgMetaData{..} = runResourceT $ do
+    (hin, hout) <- interactCapnp "encode" meta
+    lift $ do
+        forkIO $ do
+            hPutStr hin msgValue
+            hClose hin
+        hSetBinaryMode hout True
+        BS.hGetContents hout
+
+-- | @capnpDecode msg meta@ runs @capnp encode@ on the message, providing
+-- the needed metadata and returning the output
+capnpDecode :: BS.ByteString -> MsgMetaData -> IO String
+capnpDecode msgValue meta@MsgMetaData{..} = runResourceT $ do
+    schemaFile <- saveTmpSchema msgSchema
+    (hin, hout) <- interactCapnp "decode" meta
+    lift $ do
+        forkIO $ do
+            hSetBinaryMode hin True
+            BS.hPutStr hin msgValue
+            hClose hin
+        hGetContents hout
+
+-- | A helper for @capnpEncode@ and @capnpDecode@. Launches the capnp command
+-- with the given subcommand (either "encode" or "decode") and metadata,
+-- returning handles to its standard in and standard out. This runs inside
+-- ResourceT, and sets the handles up to be freed when the ResourceT exits.
+interactCapnp :: String -> MsgMetaData -> ResourceT IO (Handle, Handle)
+interactCapnp subCommand MsgMetaData{..} = do
+    schemaFile <- saveTmpSchema msgSchema
+    let p = (proc "capnp" [ subCommand
+                          , schemaFile
+                          , msgType
+                          ]) { std_in = CreatePipe
+                             , std_out = CreatePipe
+                             }
+    (_, (Just hin, Just hout, Nothing, _)) <- allocate
+        (createProcess p)
+        (\(Just hin, Just hout, Nothing, _p) -> do
+            hClose hout
+            hClose hin)
+    return (hin, hout)
+
+
+saveTmpSchema msgSchema = snd <$> allocate writeTempFile removeFile
   where
-    writeTempFile template contents = runResourceT $ do
+    writeTempFile = runResourceT $ do
         (_, (path, hndl)) <- allocate
-            (openTempFile "/tmp" template)
+            (openTempFile "/tmp" "schema.capnp")
             (\(_, hndl) -> hClose hndl)
-        lift $ hPutStr hndl contents
+        lift $ hPutStr hndl msgSchema
         return path
-    runCapnp schemaFile = do
-        let p = (proc "capnp" [ "encode"
-                              , schemaFile
-                              , msgType
-                              ]) { std_in = CreatePipe
-                                 , std_out = CreatePipe
-                                 }
-        (_, (Just hin, Just hout, Nothing, _)) <- allocate
-            (createProcess p)
-            (\(Just hin, Just hout, Nothing, _p) -> do
-                hClose hout
-                hClose hin)
-        lift $ do
-            forkIO $ do
-                hPutStr hin msgValue
-                hClose hin
-            hSetBinaryMode hout True
-            BS.hGetContents hout
 
 
 -- | Convert a list of 'Assertion's to a test group with the given name.
