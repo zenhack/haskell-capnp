8283bae5c7367cb0bbb0b75151bf7ca8c0fc1d89
Ian Denhardt <ian@zenhack.net>
2017-08-27 20:34:25 -0400

    Fix types in schema schema.
    * Add explicit `Maybe`s to the pointer fields.
    * Fix the type of `displayNamePrefixLength`; this was probably
      a transcription error.

--- Schema/CapNProto/Schema/CodeGeneratorRequest.hs
+++ Schema/CapNProto/Schema/CodeGeneratorRequest.hs
@@ -5,8 +5,8 @@ import qualified Schema.CapNProto.Schema as S
 
 data RequestedFile = RequestedFile
 
-nodes :: DS.Field S.CodeGeneratorRequest (DS.List S.Node)
+nodes :: DS.Field S.CodeGeneratorRequest (Maybe (DS.List S.Node))
 nodes = DS.PtrField 0
 
-requestedFiles :: DS.Field S.CodeGeneratorRequest (DS.List RequestedFile)
+requestedFiles :: DS.Field S.CodeGeneratorRequest (Maybe (DS.List RequestedFile))
 requestedFiles = DS.PtrField 1
--- Schema/CapNProto/Schema/Node.hs
+++ Schema/CapNProto/Schema/Node.hs
@@ -9,25 +9,25 @@ import qualified Schema.CapNProto.Schema as S
 id :: DS.Field S.Node S.Id
 id = DS.fromBits 0 64
 
-displayName :: DS.Field S.Node Text
+displayName :: DS.Field S.Node (Maybe Text)
 displayName = DS.PtrField 0
 
-displayNamePrefixLength :: DS.Field S.Node Text
+displayNamePrefixLength :: DS.Field S.Node Word32
 displayNamePrefixLength = DS.fromBits 64 96
 
 scopeId :: DS.Field S.Node S.Id
 scopeId = DS.fromBits 128 192
 
-parameters :: DS.Field S.Node (DS.List Parameter)
+parameters :: DS.Field S.Node (Maybe (DS.List Parameter))
 parameters = DS.PtrField 5
 
 isGeneric :: DS.Field S.Node Bool
 isGeneric = DS.fromBits 288 289
 
-nestedNodes :: DS.Field S.Node (DS.List NestedNode)
+nestedNodes :: DS.Field S.Node (Maybe (DS.List NestedNode))
 nestedNodes = DS.PtrField 1
 
-annotations :: DS.Field S.Node (DS.List S.Annotation)
+annotations :: DS.Field S.Node (Maybe (DS.List S.Annotation))
 annotations = DS.PtrField 2
 
 data Parameter = Parameter
