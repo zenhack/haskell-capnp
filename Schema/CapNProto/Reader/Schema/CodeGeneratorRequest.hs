49050ca235f186f6019ddfe11f8388e5604f20e8
Ian Denhardt <ian@zenhack.net>
2017-09-03 18:17:56 -0400

    Remove some unused imports

--- Schema/CapNProto/Reader/Schema/CodeGeneratorRequest.hs
+++ Schema/CapNProto/Reader/Schema/CodeGeneratorRequest.hs
@@ -1,12 +1,7 @@
 {-# LANGUAGE ConstraintKinds, TemplateHaskell #-}
 module Schema.CapNProto.Reader.Schema.CodeGeneratorRequest where
 
-import Control.Monad.Catch (MonadThrow)
-import Control.Monad.Quota (MonadQuota)
-
-import Data.CapNProto.Blob (Blob)
 import qualified Schema.CapNProto.Reader.Schema as S
-import qualified Data.CapNProto.Untyped as U
 
 import Data.CapNProto.TH
 
