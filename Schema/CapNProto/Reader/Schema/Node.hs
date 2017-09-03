e97637149ba9386284bd734be60fc33cffddf741
Ian Denhardt <ian@zenhack.net>
2017-09-03 18:25:35 -0400

    Start stubbing out Node type.

---
+++ Schema/CapNProto/Reader/Schema/Node.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Schema.CapNProto.Reader.Schema.Node where
+
+import Data.CapNProto.TH
+import qualified Schema.CapNProto.Reader.Schema as S
+
+$(mkStructWrappers ["Parameter", "NestedNode"])
+
+$(mkListReaders 'S.Node
+    [ ("parameters",  'Parameter)
+    , ("nestedNodes", 'NestedNode)
+    , ("annotations", 'S.Annotation)
+    , ("fields",      'S.Field)
+    ])
