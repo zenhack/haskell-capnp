ffaba90a0916eb0e90dd7711a6e7e9b08f170749
Ian Denhardt <ian@zenhack.net>
2017-09-03 20:17:15 -0400

    mkListReaders: generate type signatures
    This silences a warning that was causing a lot of noise

--- Data/CapNProto/TH.hs
+++ Data/CapNProto/TH.hs
@@ -6,9 +6,18 @@ module Data.CapNProto.TH
   where
 
 import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
 import Data.Word
 
 import qualified Data.CapNProto.Untyped as U
+
+-- | For a type with one data constructor, with the same name as its type
+-- constructor, convert a 'Name' for the data constructor to a 'Name' for
+-- the type constructor.
+inferTypeName :: Name -> Name
+inferTypeName (Name occ (NameG DataName pkgName modName)) =
+    Name occ (NameG TcClsName pkgName modName)
+inferTypeName name = name
 
 mkStructWrapper :: String -> DecQ
 mkStructWrapper name = do
@@ -25,17 +34,28 @@ mkStructWrapper name = do
 mkStructWrappers :: [String] -> DecsQ
 mkStructWrappers = mapM mkStructWrapper
 
-mkListReader :: String -> Word16 -> Name -> Name -> DecQ
-mkListReader name offset parentType childType = do
+mkListReader :: String -> Word16 -> Name -> Name -> DecsQ
+mkListReader name offset parentData childData = do
+    let parentType = inferTypeName parentData
+    let childType = inferTypeName childData
     let fnName = mkName name
     struct <- newName "struct"
     body <- mkBody struct
-    return $ FunD fnName [Clause [ConP parentType [VarP struct]]
-                                 (NormalB body)
-                                 []
-                         ]
+    m <- newName "m"
+    b <- newName "b"
+    let m' = return $ VarT m
+    let b' = return $ VarT b
+    ty <- [t| U.ReadCtx $m' $b'
+              => $(return $ ConT parentType) $b'
+              -> $m' (Maybe (U.ListOf $b' ($(return $ ConT childType) $b'))) |]
+    return $ [ SigD fnName ty
+             , FunD fnName [Clause [ConP parentData [VarP struct]]
+                                   (NormalB body)
+                                   []
+                           ]
+             ]
  where
-    childCon = return $ ConE childType
+    childCon = return $ ConE childData
     mkBody struct = do
         [| do ptr <- U.ptrSection $(return $ VarE struct) >>= U.index offset
               case ptr of
@@ -45,5 +65,7 @@ mkListReader name offset parentType childType = do
                         return $ Just $ fmap $(childCon) listPtr |]
 
 mkListReaders :: Name -> [(String, Name, Word16)] -> DecsQ
-mkListReaders parent = mapM mkReader where
+mkListReaders parent readers = do
+    concat <$> mapM mkReader readers
+  where
     mkReader (name, child, offset) = mkListReader name offset parent child
