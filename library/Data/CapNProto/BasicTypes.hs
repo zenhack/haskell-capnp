d2fee5369ac77ce56df211187be3ab9b2b41c93a
Ian Denhardt <ian@zenhack.net>
2017-09-23 00:52:34 -0400

    Functions for extracting Text/Data data types.

--- library/Data/CapNProto/BasicTypes.hs
+++ library/Data/CapNProto/BasicTypes.hs
@@ -8,12 +8,44 @@
 module Data.CapNProto.BasicTypes where
 
 import Data.Word
+
+import Control.Monad (when)
+import Control.Monad.Catch (throwM)
+
+import qualified Data.CapNProto.Blob    as B
+import qualified Data.CapNProto.Errors  as E
 import qualified Data.CapNProto.Untyped as U
 
--- | A textual string. On the wire, this is NUL-terminated. The encoding
--- should be UTF-8, but the library *does not* verify this; users of the
--- library must do validation themselves, if they care about this.
-newtype Text b = Text (U.ListOf b Word8)
+-- | A textual string ("Text" in capnproto's schema language). On the wire,
+-- this is NUL-terminated. The encoding should be UTF-8, but the library *does
+-- not* verify this; users of the library must do validation themselves, if
+-- they care about this.
+--
+-- The argument to the constructor is the slice of the original message
+-- containing the text (but not the NUL terminator).
+newtype Text b = Text b
 
--- | A blob of bytes.
-newtype Data b = Data (U.ListOf b Word8)
+-- | A blob of bytes ("Data" in capnproto's schema language). The argument
+-- to the constructor is a slice into the message, containing the raw bytes.
+newtype Data b = Data b
+
+-- | Interpret a list of Word8 as a capnproto 'Data' value. This validates that
+-- the bytes are in-bounds, throwing a 'BoundsError' if not.
+getData :: (U.ReadCtx m b, B.Slice m b) => U.ListOf b Word8 -> m (Data b)
+getData list = Data <$> U.rawBytes list
+
+-- | Interpret a list of Word8 as a capnproto 'Text' value.
+--
+-- This vaidates that the list is in-bounds, and that it is NUL-terminated,
+-- but not that it is valid UTF-8. If it is not NUL-terminaed, a x
+-- 'SchemaViolationError' is thrown.
+getText :: (U.ReadCtx m b, B.Slice m b) => U.ListOf b Word8 -> m (Text b)
+getText list = do
+    bytes <- rawBytes list
+    len <- B.length bytes
+    when (len == 0) $ throwM $ SchemaViolationError
+        "Text is not NUL-terminated (list of bytes has length 0)"
+    lastByte <- U.index (len - 1) bytes
+    when (lastByte /= 0) $ throwM $ SchemaViolationError $
+        "Text is not NUL-terminated (last byte is " ++ show lastByte ++ ")"
+    Text <$> B.slice bytes 0 (len - 1)
