01c944326d37e095990f1fceae6e6774c413c5cd
Ian Denhardt <ian@zenhack.net>
2017-09-29 23:57:39 -0400

    Start playing with making capnpc-haskell actually do something.
    I'm going to start stubbing the actual code generator, filling in gaps
    in the support libs as I go -- they're far enough along that it seems
    worth gaining some experience.

--- executables/capnpc-haskell/Main.hs
+++ executables/capnpc-haskell/Main.hs
@@ -1,12 +1,21 @@
 module Main (main) where
 
+import Control.Monad.Quota (evalQuotaT, Quota(..))
+
 import qualified Data.ByteString        as BS
 import qualified Data.CapNProto.Message as M
+import qualified Data.CapNProto.Untyped as U
+import qualified Schema.CapNProto.Reader.Schema.CodeGeneratorRequest as CGReq
+
+
+codegen :: U.ReadCtx m b => M.Message b -> m ()
+codegen msg = do
+    root <- CGReq.root_ msg
+    return ()
 
 main :: IO ()
 main = do
     contents <- BS.getContents
-    print $ BS.length contents
-    print contents
     msg <- M.decode contents
-    print msg
+    result <- evalQuotaT (codegen msg) (Quota $ BS.length contents * 10)
+    print result
