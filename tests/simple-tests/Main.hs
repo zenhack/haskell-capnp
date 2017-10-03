6bef745ef3fe2061e13f1231e0cea004df9553b9
Ian Denhardt <ian@zenhack.net>
2017-10-02 23:46:30 -0400

    Add (failing) test.
    List.mapM_ doesn't seem to actually walk over the list. Caught this
    while experimenting.

--- tests/simple-tests/Main.hs
+++ tests/simple-tests/Main.hs
@@ -6,6 +6,7 @@ import Tests.EncodeDecodeUntyped
     (encodeDecodeUntypedTests)
 import Tests.Module.Control.Monad.CapNProto.MessageBuilder (buildTests)
 import Tests.Module.Data.CapNProto.Bits                    (bitsTests)
+import Tests.Module.Data.CapNProto.List                    (listTests)
 import Tests.Module.Data.CapNProto.Pointer                 (ptrTests)
 import Tests.Module.Data.CapNProto.Untyped                 (untypedTests)
 import Tests.WalkSchemaCodeGenRequest
@@ -15,6 +16,7 @@ main :: IO ()
 main = defaultMain [ bitsTests
                    , ptrTests
                    , untypedTests
+                   , listTests
                    , buildTests
                    , encodeDecodeUntypedTests
                    , walkSchemaCodeGenRequestTest
