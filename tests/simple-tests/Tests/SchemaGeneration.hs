6aa99f069f878ac04a0373f3c4d96bd624b647be
Remy Goldschmidt <taktoa@gmail.com>
2017-10-06 16:53:24 -0500

    refactored quickcheck code

--- tests/simple-tests/Tests/SchemaGeneration.hs
+++ tests/simple-tests/Tests/SchemaGeneration.hs
@@ -1,119 +1,133 @@
 module Tests.SchemaGeneration
   where
 
-import Test.QuickCheck
-import Control.Monad (replicateM)
+import           Control.Monad   (replicateM)
+import           Data.Foldable   (foldl')
+import qualified Test.QuickCheck as QC
 
 -- Definitions
 
-newtype FieldName = FieldName String
-instance Show (FieldName) where
+newtype FieldName
+  = FieldName String
+
+instance Show FieldName where
   show (FieldName fn) = fn
 
-newtype StructName = StructName String
-instance Show (StructName) where
+newtype StructName
+  = StructName String
+
+instance Show StructName where
   show (StructName fn) = fn
 
-data Field = FieldDef FieldName Int FieldType
-           | StructDef StructName [Field]
+data Field
+  = FieldDef FieldName Int FieldType
+  | StructDef StructName [Field]
 
-data BuiltIn = Void
-             | Bool
-             | Int8 | Int16 | Int32 | Int64
-             | UInt8 | UInt16 | UInt32 | UInt64
-             | Float32 | Float64
-             | Text
-             | Data
+data BuiltIn
+  = Void
+  | Bool
+  | Int8
+  | Int16
+  | Int32
+  | Int64
+  | UInt8
+  | UInt16
+  | UInt32
+  | UInt64
+  | Float32
+  | Float64
+  | Text
+  | Data
   deriving (Show, Enum)
 
-data FieldType = BasicType BuiltIn
-               | ListType FieldType
-               | StructType StructName
-instance Show (FieldType) where
-  show (BasicType bi) = show bi
-  show (ListType ft) = "List(" ++ (show ft) ++ ")"
+data FieldType
+  = BasicType BuiltIn
+  | ListType FieldType
+  | StructType StructName
+
+instance Show FieldType where
+  show (BasicType bi)  = show bi
+  show (ListType ft)   = "List(" ++ show ft ++ ")"
   show (StructType sn) = show sn
 
-instance Show (Field) where
-  show (FieldDef name order entryType) =
-    (show name) ++ " @" ++ (show order) ++ " :" ++
-    (show entryType) ++ ";\n"
-  show (StructDef name content) =
-    "struct " ++ (show name) ++ " {\n"
-    ++ (foldl (++) "" ["\t" ++ (show se) | se <- content])
-    ++ "}\n\n"
+instance Show Field where
+  show (FieldDef name order entryType) = concat
+                                         [ show name, " @", show order, " :"
+                                         , show entryType, ";\n"
+                                         ]
+  show (StructDef name content)        = concat
+                                         [ "struct ", show name, " {\n"
+                                         , concatMap (('\t':) . show) content
+                                         , "}\n\n"
+                                         ]
 
-data Schema = Schema
-  { schemaId :: String
-  , schemaContent :: [Field]
-  }
-instance Show (Schema) where
-  show s = "@0x" ++ (schemaId s) ++ ";\n\n"
-    ++ (foldr (++) "" [show st | st <- schemaContent s])
+data Schema
+  = Schema
+    { schemaId      :: String
+    , schemaContent :: [Field]
+    }
+
+instance Show Schema where
+  show s = concat
+           [ "@0x", schemaId s, ";\n\n"
+           , concatMap show (schemaContent s)
+           ]
 
 -- Helper generators
 
-genSafeLCChar :: Gen Char
-genSafeLCChar = elements ['a'..'z']
+genSafeLCChar :: QC.Gen Char
+genSafeLCChar = QC.elements ['a'..'z']
 
-genSafeUCChar :: Gen Char
-genSafeUCChar = elements ['A'..'Z']
+genSafeUCChar :: QC.Gen Char
+genSafeUCChar = QC.elements ['A'..'Z']
 
-genSafeHexChar :: Gen Char
-genSafeHexChar = elements (['0'..'9'] ++ ['a'..'f'])
-
-genEnumerated :: Gen a -> Gen [a]
-genEnumerated gen = sized $ \n -> do
-  k <- choose (1, 1 `max` n)
-  replicateM k gen
+genSafeHexChar :: QC.Gen Char
+genSafeHexChar = QC.elements (['0'..'9'] ++ ['a'..'f'])
 
 -- Field types
 
 -- need to enumerate each field; this will be performed during struct
 -- generation where the number of fields is known (numberDefs)
-genFieldDef :: [FieldType] -> Int -> Gen Field
+genFieldDef :: [FieldType] -> Int -> QC.Gen Field
 genFieldDef structTypes order = do
   fieldName <- do
-    str <- listOf1 genSafeLCChar
+    str <- QC.listOf1 genSafeLCChar
     return $ FieldName (str ++ show order)
-  fieldType <- elements ((map BasicType [Bool ..]) ++ structTypes)
-  
+  fieldType <- QC.elements ((map BasicType [Bool ..]) ++ structTypes)
+
   return $ FieldDef fieldName order fieldType
 
 -- Struct type
 
 -- like fields, we enumerate each struct during generation for uniqueness
-genStructDef :: Int -> Int -> Gen Field
+genStructDef :: Int -> Int -> QC.Gen Field
 genStructDef depth order = do
   -- generate the struct's name
   structName <- do
     fc <- genSafeUCChar
-    rest <- listOf genSafeLCChar
+    rest <- QC.listOf genSafeLCChar
     return $ StructName ((fc:rest) ++ (show order))
 
   -- generate the nested structs
   structDefIndices <- if depth == 0
                       then pure []
-                      else (\n -> [0..n]) <$> choose (0, 3)
+                      else (\n -> [0..n]) <$> QC.choose (0, 3)
   structDefs <- mapM (genStructDef (depth - 1)) structDefIndices
 
   -- extract the available struct types
   let structTypes = map (\(StructDef sn _) -> (StructType sn)) structDefs
 
   -- generate the fields using available struct types
-  numFieldDefs <- sized (\n -> choose (1, 1 `max` n))
+  numFieldDefs <- QC.sized (\n -> QC.choose (1, 1 `max` n))
   fieldDefs <- mapM (genFieldDef structTypes) [0..numFieldDefs]
 
   return $ StructDef structName (fieldDefs ++ structDefs)
 
   -- Schema type
 
-genSchema :: Gen Schema
+genSchema :: QC.Gen Schema
 genSchema = do
-  id1st <- elements ['a'..'f']
-  idrest <- vectorOf 15 genSafeHexChar
+  id1st <- QC.elements ['a'..'f']
+  idrest <- QC.vectorOf 15 genSafeHexChar
   content <- genStructDef 3 0 -- multiple structs make tests take too long
   return $ Schema (id1st:idrest) [content]
-
-instance Arbitrary Schema where
-  arbitrary = genSchema
--- tests/simple-tests/Tests/SchemaQuickCheck.hs
+++ tests/simple-tests/Tests/SchemaQuickCheck.hs
@@ -4,20 +4,21 @@ module Tests.SchemaQuickCheck
 
 import qualified Data.ByteString as BS
 
-import qualified Data.CapNProto.Untyped as Untyped
-import Data.CapNProto.Blob (BlobSlice)
-import Data.CapNProto.Message as M
-import Schema.CapNProto.Reader.Schema as Schema hiding (Field)
+import           Data.CapNProto.Blob                                 (BlobSlice)
+import           Data.CapNProto.Message                              as M
+import qualified Data.CapNProto.Untyped                              as Untyped
+import           Schema.CapNProto.Reader.Schema                      as Schema hiding
+    (Field)
 import qualified Schema.CapNProto.Reader.Schema.CodeGeneratorRequest as CGReq
 
 -- Testing framework imports
-import Test.QuickCheck
-import Test.Framework (Test)
+import Test.Framework                       (Test)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.QuickCheck
 
 -- Schema generation imports
-import Tests.Util
 import Tests.SchemaGeneration
+import Tests.Util
 
 -- Schema validation imports
 import Control.Monad.Catch as C
@@ -34,16 +35,15 @@ generateCGR schema = do
 
 decodeCGR :: BS.ByteString -> IO (Quota, Int)
 decodeCGR bytes = do
+  let reader :: Untyped.Struct BS.ByteString -> QuotaT IO Int
+      reader struct = do
+        let req = Schema.CodeGeneratorRequest struct
+        Just nodes <- CGReq.nodes req
+        Just requestedFiles <- CGReq.requestedFiles req
+        return (Untyped.length nodes)
   msg <- M.decode bytes
   (numNodes, endQuota) <- runQuotaT (Untyped.rootPtr msg >>= reader) 1024
   return (endQuota, numNodes)
-  where
-    reader :: Untyped.Struct BS.ByteString -> QuotaT IO Int
-    reader struct = do
-      let req = Schema.CodeGeneratorRequest struct
-      Just nodes <- CGReq.nodes req
-      Just requestedFiles <- CGReq.requestedFiles req
-      return (Untyped.length nodes)
 
 -- QuickCheck properties
 
@@ -53,6 +53,7 @@ prop_schemaValid schema = ioProperty $ do
   decoded <- try $ decodeCGR compiled
   return $ case decoded of
     Left QuotaError -> False
-    Right _ -> True
+    Right _         -> True
 
-schemaCGRQuickCheck = testProperty "valid schema QuickCheck" prop_schemaValid 
+schemaCGRQuickCheck = testProperty "valid schema QuickCheck"
+                      (prop_schemaValid <$> genSchema)
