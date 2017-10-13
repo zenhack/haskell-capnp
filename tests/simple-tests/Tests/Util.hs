bb982b408c4ebb82ad56961a957cd67ab51f80c1
Andrew D'Angelo <dangeloandrew@outlook.com>
2017-10-13 11:49:52 -0500

    make interactCapnp return an exit code as well

--- tests/simple-tests/Tests/Util.hs
+++ tests/simple-tests/Tests/Util.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeFamilies    #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Tests.Util
     ( MsgMetaData(..)
     , capnpEncode, capnpDecode, capnpCompile, capnpId
@@ -40,44 +41,62 @@ data MsgMetaData = MsgMetaData
 -- | @capnpEncode msg meta@ runs @capnp encode@ on the message, providing
 -- the needed metadata and returning the output
 capnpEncode :: String -> MsgMetaData -> IO BS.ByteString
-capnpEncode msgValue meta = runResourceT $ do
-    (hin, hout) <- interactCapnpWithSchema "encode" meta
-    lift $ do
-        forkIO $ do
-            hPutStr hin msgValue
-            hClose hin
-        hSetBinaryMode hout True
-        BS.hGetContents hout
+capnpEncode msgValue meta = do
+  (exitCode, contents) <- runResourceT $ do
+    (hin, hout, phandle) <- interactCapnpWithSchema "encode" meta
+    contents <- lift $ do
+      forkIO $ do
+        hPutStr hin msgValue
+        hClose hin
+      hSetBinaryMode hout True
+      BS.hGetContents hout
+    exitCode <- lift $ waitForProcess phandle
+    return (exitCode, contents)
+  -- check exit code here
+  return contents
 
 -- | @capnpDecode msg meta@ runs @capnp encode@ on the message, providing
 -- the needed metadata and returning the output
 capnpDecode :: BS.ByteString -> MsgMetaData -> IO String
-capnpDecode msgValue meta = runResourceT $ do
-    (hin, hout) <- interactCapnpWithSchema "decode" meta
-    lift $ do
-        forkIO $ do
-            hSetBinaryMode hin True
-            BS.hPutStr hin msgValue
-            hClose hin
-        ret <- hGetContents hout
-        -- We need to read the whole string in strictly, otherwise hClose may
-        -- happen before we're done; use deepseq to force full evaluation:
-        deepseq ret (return ret)
+capnpDecode msgValue meta = do
+  (exitCode, contents) <- runResourceT $ do
+    (hin, hout, phandle) <- interactCapnpWithSchema "decode" meta
+    contents <- lift $ do
+      forkIO $ do
+        hSetBinaryMode hin True
+        BS.hPutStr hin msgValue
+        hClose hin
+      ret <- hGetContents hout
+      -- We need to read the whole string in strictly, otherwise hClose may
+      -- happen before we're done; use deepseq to force full evaluation:
+      deepseq ret (return ret)
+    exitCode <- lift $ waitForProcess phandle
+    return (exitCode, contents)
+  -- check exit code here
+  return contents
 
 -- | @capnpCompile msg meta@ runs @capnp compile@ on the schema, providing
 -- the needed metadata and returning the output
 capnpCompile :: MsgMetaData -> IO BS.ByteString
-capnpCompile meta = runResourceT $ do
-    (hin, hout) <- interactCapnpWithSchema "compile" meta
-    lift $ do
-        hSetBinaryMode hout True
-        BS.hGetContents hout
+capnpCompile meta = do
+  (exitCode, contents) <- runResourceT $ do
+    (hin, hout, phandle) <- interactCapnpWithSchema "compile" meta
+    contents <- lift $ do
+      hSetBinaryMode hout True
+      BS.hGetContents hout
+    exitCode <- lift $ waitForProcess phandle
+    return (exitCode, contents)
+  -- check exit code here
+  return contents
 
 capnpId :: IO BS.ByteString
 capnpId = do
-  cid <- runResourceT $ do
-    (hin, hout) <- interactCapnp ["id"]
-    lift $ BS.hGetContents hout
+  (exitCode, cid) <- runResourceT $ do
+    (hin, hout, phandle) <- interactCapnp ["id"]
+    contents <- lift $ BS.hGetContents hout
+    exitCode <- lift $ waitForProcess phandle
+    return (exitCode, contents)
+  -- check exit code here
   case BSC8.stripSuffix "\n" cid of
     Just stripped -> do
       when (BSC8.length stripped /= 19)
@@ -89,25 +108,25 @@ capnpId = do
       return stripped
     Nothing -> fail "`capnp id` did not end with a newline"
 
-interactCapnp :: [String] -> ResourceT IO (Handle, Handle)
+interactCapnp :: [String] -> ResourceT IO (Handle, Handle, ProcessHandle)
 interactCapnp args = do
   let p = (proc "capnp" args) { std_in = CreatePipe
                               , std_out = CreatePipe
                               }
-  (_, (Just hin, Just hout, Nothing, _)) <- allocate
+  (_, (Just hin, Just hout, Nothing, phandle)) <- allocate
       (createProcess p)
       (\(Just hin, Just hout, Nothing, proc) -> do
           hClose hout
           hClose hin
           void $ waitForProcess proc)
-  return (hin, hout)
+  return (hin, hout, phandle)
 
 -- | A helper for @capnpEncode@ and @capnpDecode@. Launches the capnp command
 -- with the given subcommand (either "encode" or "decode") and metadata,
 -- returning handles to its standard in and standard out. This runs inside
 -- ResourceT, and sets the handles up to be closed and the process to be reaped
 -- when the ResourceT exits.
-interactCapnpWithSchema :: String -> MsgMetaData -> ResourceT IO (Handle, Handle)
+interactCapnpWithSchema :: String -> MsgMetaData -> ResourceT IO (Handle, Handle, ProcessHandle)
 interactCapnpWithSchema subCommand MsgMetaData{..} = do
   let writeTempFile = runResourceT $ do
         (_, (path, hndl)) <- allocate
