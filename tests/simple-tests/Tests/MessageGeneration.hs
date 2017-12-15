b3ee1459e42d69fe1c139f42ddda56443986c4a6
Andrew D'Angelo <dangeloandrew@outlook.com>
2017-12-15 15:56:10 -0600

    generate non-struct message items

--- tests/simple-tests/Tests/MessageGeneration.hs
+++ tests/simple-tests/Tests/MessageGeneration.hs
@@ -15,12 +15,13 @@ import qualified Data.List.NonEmpty as NE
 import Control.Monad.State.Strict
 
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC8
 
 import qualified Tests.SchemaGeneration as SG
 
 -- Definitions
 
-data BuiltInData
+data BasicData
     = Void
     | Bool Bool
     | Int Int
@@ -28,44 +29,90 @@ data BuiltInData
     | Text String
     | Data BS.ByteString
 
-instance Show BuiltInData where
+instance Show BasicData where
     show (Int i) = show i
     show (Bool b) = show b
     show (Float f) = show f
     show (Text t) = t
     show (Data d) = show d
 
+data MessageContent
+    = BasicContent BasicData
+    | ListContent [MessageContent]
+    | StructContent [MessageItem]
+
+joinStr :: String -> [String] -> String
+joinStr sep [] = ""
+joinStr sep (x:[]) = x
+joinStr sep (x:xs) = x ++ sep ++ (joinStr sep xs)
+instance Show MessageContent where
+    show (BasicContent bc) = show bc
+    show (ListContent lc) = "[" ++ (joinStr "," $ map show lc) ++ "]"
+    show (StructContent sc) = "(" ++ (joinStr "," $ map show sc) ++ ")"
+
 data MessageItem
-    = BuiltInItem SG.FieldName BuiltInData
-    | StructItem SG.StructName [MessageItem]
+    = MessageItem SG.FieldName MessageContent
 
 instance Show MessageItem where
-    show (BuiltInItem (SG.FieldName fn) bid) = fn ++ " = " ++ show bid
-    show (StructItem _ mis) = "(" ++ (join "," $ map show mis) ++ ")"
-        where join sep [] = ""
-              join sep (x:[]) = x
-              join sep (x:xs) = x ++ sep ++ (join sep xs)
+    show (MessageItem (SG.FieldName fn) mc) = fn ++ " = " ++ show mc
 
 data Message
     = Message
         { msgSchema :: SG.Schema
-        , msgData   :: MessageItem
+        , msgItem   :: MessageItem
         }
 
 instance Show Message where
     show (Message _ md) = show md
 
+-- MessageItem types
+genBasicContent :: SG.BuiltIn -> QC.Gen BasicData
+genBasicContent bt = do
+    return $ case bt of
+        SG.Void    -> undefined
+        SG.Bool    -> Bool False
+        SG.Int8    -> Int 0
+        SG.Int16   -> Int 0
+        SG.Int32   -> Int 0
+        SG.Int64   -> Int 0
+        SG.UInt8   -> Int 0
+        SG.UInt16  -> Int 0
+        SG.UInt32  -> Int 0
+        SG.UInt64  -> Int 0
+        SG.Float32 -> Float 0.0
+        SG.Float64 -> Float 0.0
+        SG.Text    -> Text "text"
+        SG.Data    -> Data (BSC8.pack "data")
+
+genMessageContent :: SG.FieldType -> QC.Gen MessageContent
+genMessageContent ft = 
+    case ft of
+        SG.BasicType bi -> do
+            bc <- genBasicContent bi
+            return $ BasicContent bc
+        SG.ListType it -> do
+            lc <- genMessageContent it
+            return $ ListContent [lc]
+        SG.StructType (SG.StructName sn) -> do
+            return $ BasicContent $ Text ("lookup: " ++ sn)
+genMessageContent _ = undefined
+
+genMessageItem :: SG.Field -> QC.Gen MessageItem
+genMessageItem (SG.FieldDef fn _ ft) = do
+    mc <- genMessageContent ft
+    return $ MessageItem fn mc
+genMessageItem (SG.StructDef (SG.StructName sn) fds) = do
+    return $ MessageItem (SG.FieldName "new_struct_def") $ BasicContent $ Text sn
+
+genFromSchema :: SG.Schema -> QC.Gen MessageItem
+genFromSchema (SG.Schema sid sfs) = do
+    items <- mapM genMessageItem sfs
+    return $ head items
+
+
 -- Message type
 genMessage :: QC.Gen Message
 genMessage = do
-    let sampleSchema = SG.Schema "aa6fda3b444d262c"
-            [ SG.StructDef (SG.StructName "A")
-                [ SG.FieldDef (SG.FieldName "a") 0 (SG.BasicType SG.UInt64)
-                , SG.FieldDef (SG.FieldName "b") 1 (SG.BasicType SG.Bool)
-                ]
-            ]
-    let sampleData = StructItem (SG.StructName "A")
-            [ BuiltInItem (SG.FieldName "a") (Int 72)
-            , BuiltInItem (SG.FieldName "b") (Bool True)
-            ]
-    return $ Message sampleSchema sampleData
+    msgSchema <- SG.genSchema
+    msgItem <- genFromSchema msgSchema
+    return $ Message msgSchema msgItem
--- tests/simple-tests/Tests/MessageQuickCheck.hs
+++ tests/simple-tests/Tests/MessageQuickCheck.hs
@@ -2,24 +2,12 @@ module Tests.MessageQuickCheck
     (msgEncodeDecodeQuickCheck)
     where
 
-import qualified Data.ByteString as BS
-
-import           Data.CapNProto.Blob                                 (BlobSlice)
-import qualified Data.CapNProto.Message                              as M
-import qualified Data.CapNProto.Untyped                              as Untyped
-
 -- Testing framework imports
-import Test.Framework                       (Test)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
 
 -- Schema generation imports
 import Tests.MessageGeneration as MG
-import Tests.Util
-
--- Schema validation imports
-import Control.Monad.Catch as C
-import Control.Monad.Quota as Q
 
 -- QuickCheck properties
 
