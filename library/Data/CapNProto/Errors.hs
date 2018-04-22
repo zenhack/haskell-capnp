8424edf271387e66ccff5a3f5393613ced4002f2
Ian Denhardt <ian@zenhack.net>
2018-04-22 18:06:13 -0400

    Add our own error type class.
    Another part of moving towards being able to do reading in parallel; we
    don't want the @throwM e >> x == throwM e@ constraint.

--- library/Data/CapNProto/Errors.hs
+++ library/Data/CapNProto/Errors.hs
@@ -2,12 +2,35 @@
 Module: Data.CapNProto.Errors
 Description: Exception types used by haskell-capnp
 -}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Data.CapNProto.Errors
     ( Error(..)
+    , ThrowError(..)
     )
   where
 
-import Control.Monad.Catch (Exception)
+import Control.Monad.Catch (Exception, MonadThrow(throwM))
+
+-- | Similar MonadThrow, but:
+--
+-- 1. Only require Applicative.
+-- 2. Don't allow general exceptions, just the 'Error' type defined in
+--    this module.
+-- 3. Don't necessarily enforce evaluation order to the same extent;
+--    while @throwM e >> m@ is required to be equivalent to @throwM e@,
+--    it is legal (though not required) for e.g:
+--
+--      @snd <$> ((,) <$> throwError e <*> pure 4@
+--
+-- to be equivalnet to @pure 4@.
+class Applicative f => ThrowError f where
+    throwError :: Error -> f a
+
+-- I don't understand why GHC can't deduce Applicative from MonadThrow,
+-- but it's giving me an error when I try to leave it out of the context.
+instance (Applicative m, MonadThrow m) => ThrowError m where
+    throwError = throwM
 
 data Error
     -- | A @BoundsError@ is an exception indicating an attempt
