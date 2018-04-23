095f6515c48db71496af34c9501e83bf384b7dbd
Ian Denhardt <ian@zenhack.net>
2018-04-23 16:18:56 -0400

    Remove all the old CI/nix goop, and the Makefile.
    I can't be bothered to maintain the Nix stuff, and it's unneeded now
    that we're doing CI with Gitlab. The Makefile was just wrong in a number
    of ways.

--- shell.nix
+++
@@ -1,4 +0,0 @@
-{ nixpkgs ? import ./nix/nixpkgs.nix, compiler ? "default", pkgName ? "capnp" }:
-
-let drv = (import ./release.nix { inherit nixpkgs compiler; }).${pkgName};
-in if builtins.getEnv "IN_NIX_SHELL" != "" then drv.env else drv
