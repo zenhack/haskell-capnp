367e103cb98b0f3f063ce68ef8f36a43be22f7e2
Ian Denhardt <ian@zenhack.net>
2018-04-26 18:00:17 -0400

    Fix a typo in a comment.

--- hlint.sh
+++ hlint.sh
@@ -2,7 +2,7 @@
 #
 # Run hlint on most of the codebase.
 #
-# We skip the generated schema, since we deliberate use conventions
+# We skip the generated schema, since we deliberately use conventions
 # in the output that hlint will flag. We also skip code that is meant
 # to be replaced by the generated output, once the generator is complete.
 exec hlint $(find * -name '*.hs' \
