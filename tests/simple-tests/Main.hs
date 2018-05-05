0356fd761fa9428a2644192c4da215b2e4f0dddf
Ian Denhardt <ian@zenhack.net>
2018-05-05 02:38:33 -0400

    Remove old template haskell based codegen.
    Note that some of the related tests have been disabled, but not deleted
    from the source tree -- adapting them to the new framework may be a bit
    of work (which I don't want to do just now), but it seems worth keeping
    them around and porting them later on.

--- tests/simple-tests/Main.hs
+++ tests/simple-tests/Main.hs
@@ -6,24 +6,26 @@ import Tests.EncodeDecodeUntyped
 import Tests.Module.Control.Monad.CapNProto.MessageBuilder (buildTests)
 import Tests.Module.Data.CapNProto.Bits                    (bitsTests)
 import Tests.Module.Data.CapNProto.Core.Schema             (schemaTests)
-import Tests.Module.Data.CapNProto.List                    (listTests)
 import Tests.Module.Data.CapNProto.Pointer                 (ptrTests)
 import Tests.Module.Data.CapNProto.Untyped                 (untypedTests)
 import Tests.Module.Data.CapNProto.Untyped.ADT             (untypedADTTests)
-import Tests.WalkSchemaCodeGenRequest
-    (walkSchemaCodeGenRequestTest)
 
-import Tests.SchemaQuickCheck (schemaCGRQuickCheck)
+-- These tests are currently broken, due to the removed TH-based code
+-- generation, but a version of them may be useful again at some point:
+--
+-- import Tests.Module.Data.CapNProto.List (listTests)
+-- import Tests.SchemaQuickCheck           (schemaCGRQuickCheck)
+-- import Tests.WalkSchemaCodeGenRequest   (walkSchemaCodeGenRequestTest)
 
 main :: IO ()
 main = defaultMain [ bitsTests
                    , ptrTests
                    , untypedTests
                    , untypedADTTests
-                   , listTests
+                   -- , listTests
                    , buildTests
                    , encodeDecodeUntypedTests
-                   , walkSchemaCodeGenRequestTest
-                   , schemaCGRQuickCheck
+                   -- , walkSchemaCodeGenRequestTest
+                   -- , schemaCGRQuickCheck
                    , schemaTests
                    ]
