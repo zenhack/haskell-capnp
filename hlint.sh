e18e1fb224336a1b332425f22a99a01c7798875f
Ian Denhardt <ian@zenhack.net>
2018-05-05 15:20:16 -0400

    hlint: don't check top-level directory.
    I've been doing some ad-hoc testing running the compiler plugin and
    writing the output to a file in $PWD (the root of the repo). But hlint
    complains about the generated code, and I run it in a pre-commit hook,
    so I have to remember to delete the file before committing. This patch
    solves this by restricting the checks to directories where code is
    actually supposed to live.
    
    I also delete the reference to the now-absent directory for the old code
    generator's output.

--- hlint.sh
+++ hlint.sh
@@ -5,6 +5,4 @@
 # We skip the generated schema, since we deliberately use conventions
 # in the output that hlint will flag. We also skip code that is meant
 # to be replaced by the generated output, once the generator is complete.
-exec hlint $(find * -name '*.hs' \
-	| grep -v Data.CapNProto.Core \
-	| grep -v Schema.CapNProto)
+exec hlint $(find lib exe tests -name '*.hs' | grep -v Data.CapNProto.Core)
