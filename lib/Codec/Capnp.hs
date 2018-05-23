72141cda72752c4dcdd9c33dcaa65e00b1211873
Ian Denhardt <ian@zenhack.net>
2018-05-23 18:53:23 -0400

    Add helpers for extracting particular kinds of pointers, with defaults.
    We will start generating code that calls these.

--- lib/Codec/Capnp.hs
+++ lib/Codec/Capnp.hs
@@ -7,6 +7,10 @@ import Data.Word
 import Control.Monad.Catch (MonadThrow(throwM))
 import Data.Bits           ((.&.))
 import Data.Capnp.Errors   (Error(SchemaViolationError))
+import Data.Capnp.Untyped
+    (List(..), ListOf, Ptr(..), ReadCtx, Struct, messageDefault)
+
+import qualified Data.Capnp.Message as M
 
 class Decerialize from to where
     decerialize :: MonadThrow m => from -> m to
@@ -52,3 +56,43 @@ instance IsWord Word32 where
 instance IsWord Word64 where
     fromWord = fromIntegral
     toWord = fromIntegral
+
+getList0 ::  ReadCtx m b => M.Message b -> Maybe (Ptr b) -> m (ListOf b ())
+getList0 msg Nothing                       = pure $ messageDefault msg
+getList0 msg (Just (PtrList (List0 list))) = pure list
+getList0 _ _ = expected "pointer to list with element size 0"
+
+getList8 ::  ReadCtx m b => M.Message b -> Maybe (Ptr b) -> m (ListOf b Word8)
+getList8 msg Nothing                       = pure $ messageDefault msg
+getList8 msg (Just (PtrList (List8 list))) = pure list
+getList8 _ _ = expected "pointer to list with element size 8"
+
+getList16 ::  ReadCtx m b => M.Message b -> Maybe (Ptr b) -> m (ListOf b Word16)
+getList16 msg Nothing                        = pure $ messageDefault msg
+getList16 msg (Just (PtrList (List16 list))) = pure list
+getList16 _ _ = expected "pointer to list with element size 16"
+
+getList32 ::  ReadCtx m b => M.Message b -> Maybe (Ptr b) -> m (ListOf b Word32)
+getList32 msg Nothing                        = pure $ messageDefault msg
+getList32 msg (Just (PtrList (List32 list))) = pure list
+getList32 _ _ = expected "pointer to list with element size 32"
+
+getList64 ::  ReadCtx m b => M.Message b -> Maybe (Ptr b) -> m (ListOf b Word64)
+getList64 msg Nothing                        = pure $ messageDefault msg
+getList64 msg (Just (PtrList (List64 list))) = pure list
+getList64 _ _ = expected "pointer to list with element size 64"
+
+getListPtr ::  ReadCtx m b => M.Message b -> Maybe (Ptr b) -> m (ListOf b (Maybe (Ptr b)))
+getListPtr msg Nothing                         = pure $ messageDefault msg
+getListPtr msg (Just (PtrList (ListPtr list))) = pure list
+getListPtr _ _ = expected "pointer to list of pointers"
+
+getListStruct ::  ReadCtx m b => M.Message b -> Maybe (Ptr b) -> m (ListOf b (Struct b))
+getListStruct msg Nothing                            = pure $ messageDefault msg
+getListStruct msg (Just (PtrList (ListStruct list))) = pure list
+getListStruct _ _ = expected "pointer to list of structs"
+
+getStruct ::  ReadCtx m b => M.Message b -> Maybe (Ptr b) -> m (Struct b)
+getStruct msg Nothing              = pure $ messageDefault msg
+getStruct msg (Just (PtrStruct s)) = pure s
+getStruct _ _                      = expected "pointer to struct"
