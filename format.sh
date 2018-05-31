535b871bbfd48029250f2b3c2b347a42afc6191d
Ian Denhardt <ian@zenhack.net>
2018-05-30 21:30:46 -0400

    Don't set -x in format.sh
    The script doesn't really need debugging anymore, and this is just
    noise.

--- format.sh
+++ format.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #
 # Format the whole source tree with stylish-haskell. Skip generated output.
-set -ex
+set -e
 cd "$(dirname $0)"
 stylish-haskell -i $(find * -name '*.hs' | grep -v Data.Capnp.ById)
