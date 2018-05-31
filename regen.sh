c177f5e6ff0e988caa32821e1e33e32bde03b6b2
Ian Denhardt <ian@zenhack.net>
2018-05-30 20:57:54 -0400

    Rework regen.sh
    The biggest difference is that we no longer copy the schema from the
    system, but generate codec directly from the copies we already have
    locally.
    
    To facilitate this, the schema need to be inside a directory called
    `capnp`, because they import one another with paths starting with
    "/capnp". Accordingly, we've moved the *.capnp files to
    `lib/schema/capnp`.
    
    Finally, we run the code generator from within lib/, so we don't have
    to then move the generated output.

--- regen.sh
+++ regen.sh
@@ -1,14 +1,46 @@
 #!/usr/bin/env sh
 #
 # Regenerate modules for the core capnproto schema.
-#
-# This does the following:
-#
-# 1. Copy the schema from /usr/include/capnp/ into the repo
-# 2. Generate code for the schema
-# 3. Copy it into place in the module hierarchy.
-set -ex
-cp /usr/include/capnp/*.capnp lib/Data/Capnp/
-capnp compile -o- /usr/include/capnp/*.capnp | cabal new-run capnpc-haskell
-find Data/ -type f -name '*.hs' -exec mv \{} lib/\{} \;
-rm -r Data/
+set -e
+
+# Some helpers for reporting info to the caller:
+log() {
+	printf '%s\n' "$@" >&2
+}
+
+err() {
+	log $@
+	exit 1
+}
+
+# First make sure the compiler plugin is up to date.
+log "Rebuilding schema compiler plugin..."
+cd "$(dirname $0)"
+cabal new-build capnpc-haskell
+
+# We run the code generator from inside lib/, so that it outputs
+# modules to the right locations:
+cd lib
+
+# Find the compiler plugin executable. It would be nice to just
+# use new-run here, but doing so from a subdirectory is a bit fiddly
+# and I(zenhack) haven't found a nice way to do it.
+exe="$(find ../dist-newstyle -type f -name capnpc-haskell)"
+
+# Make sure we only found one file:
+argslen() {
+	echo $#
+}
+case $(argslen $exe) in
+	0) err "Error: capnpc-haskell executable not found in dist-newstyle." ;;
+	1) : ;; # Just one file; we're okay.
+	*) err "Error: more than one capnpc-haskell executable found in dist-newstyle." ;;
+esac
+
+# Ok -- do the codegen. Add the compiler plugin to our path and invoke
+# capnp compile.
+log "Generating schema modules..."
+export PATH="$(dirname $exe):$PATH"
+capnp compile -I ./schema -ohaskell ./schema/capnp/*.capnp
+
+# vim: set ts=2 sw=2 noet :
