d76b3c3be7d3bfda1d96bcdb305ef28584c040b6
Ian Denhardt <ian@zenhack.net>
2018-06-17 20:29:03 -0400

    Kill some redundant code.

--- lib/Data/Capnp/Untyped/Mutable.hs
+++ lib/Data/Capnp/Untyped/Mutable.hs
@@ -1,58 +1,24 @@
-{-# LANGUAGE GADTs           #-}
-{-# LANGUAGE RecordWildCards #-}
 module Data.Capnp.Untyped.Mutable
-    ( Ptr(..), List(..), Struct, ListOf
+    (
+    -- * Special-case type aliases
+    Ptr(..), List(..), Struct(..), ListOf(..)
+    -- * Re-exported from 'Data.Capnp.Untyped.Generic':
+    , GU.dataSection, GU.ptrSection
+    , GU.getData, GU.getPtr
+    , GU.get, GU.index, GU.length
+    , GU.take
+    , GU.rootPtr
+    , GU.rawBytes
+    , GU.ReadCtx
+    , GU.HasMessage(..), GU.MessageDefault(..)
     ) where
 
 import Data.Word
 
-import qualified Data.Capnp.Message.Mutable as M
+import qualified Data.Capnp.Message.Mutable as MM
+import qualified Data.Capnp.Untyped.Generic as GU
 
-data Ptr s
-    = PtrCap (M.Message s) !Word32
-    | PtrList (List s)
-    | PtrStruct (Struct s)
-
-data List s
-    = List0 (ListOf s ())
-    | List1 (ListOf s Bool)
-    | List8 (ListOf s Word8)
-    | List16 (ListOf s Word16)
-    | List32 (ListOf s Word32)
-    | List64 (ListOf s Word64)
-    | ListPtr (ListOf s (Maybe (Ptr s)))
-    | ListStruct (ListOf s (Struct s))
-
-data Struct s = Struct
-    (M.Message s)
-    !WordAddr
-    !Word16
-    !Word16
-
-data ListOf s a where
-    ListOfVoid :: M.Message s -> !Int -> ListOf s ()
-    ListOfStruct :: Struct s -> !Int -> ListOf s (Struct s)
-    ListOfBool :: !NormalList s -> ListOf s Bool
-    ListOfWord8  :: !NormalList s -> ListOf s Word8
-    ListOfWord16 :: !NormalList s -> ListOf s Word16
-    ListOfWord32 :: !NormalList s -> ListOf s Word32
-    ListOfWord64 :: !NormalList s -> ListOf s Word64
-    ListOfPtr    :: !NormalList s -> ListOf s (Maybe (Ptr s))
-
-data NormalList = NormalList
-    { nMsg  :: M.Message s
-    , nAddr :: WordAddr
-    , nLen  :: !Int
-    }
-
-
-dataSection :: Struct s -> ListOf s Word64
-dataSection (Struct msg addr dataSz _) =
-    ListOfWord64 $ NormalList msg addr (fromIntegral dataSz)
-
-ptrSection :: Struct s -> ListOf s (Maybe (Ptr s))
-ptrSection (Struct msg addr@WordAt{..} dataSz ptrSz) =
-    ListOfPtr $ NormalList
-        msg
-        addr { wordIndex = wordIndex + fromIntegral dataSz }
-        (fromIntegral ptrSz)
+type Ptr s = GU.Ptr (MM.Message s)
+type List s = GU.List (MM.Message s)
+type Struct s = GU.Struct (MM.Message s)
+type ListOf s a  = GU.ListOf (MM.Message s) a
