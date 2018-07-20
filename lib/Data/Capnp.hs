af2138a99ebb9f4c7707d848e47445f1fb72fbb0
Ian Denhardt <ian@zenhack.net>
2018-07-20 18:45:01 -0400

    Document Get/Set/Has types.

--- lib/Data/Capnp.hs
+++ lib/Data/Capnp.hs
@@ -1,6 +1,13 @@
 module Data.Capnp where
 
-
+-- | A 'Get' is a wrapper around a @get_*@ function for a field. This is
+-- intended to be used with the OverloadedLabels extension, like
+-- @'get' #myField struct@. The newtype wrapper and 'get' function allow
+-- us to disambiguate between different uses of @#myField@.
 newtype Get m parent child = Get { get :: parent -> m child }
+
+-- | Like 'Get', but wraps a @has_*@ function instead of a @get_*@ function.
 newtype Has m parent = Has { has :: parent -> m Bool }
+
+-- | Like 'Get', but wraps a @set_*@ function instead of a @get_*@ function.
 newtype Set m parent child = Set { set :: parent -> child -> m () }
