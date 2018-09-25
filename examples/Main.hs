91e03ee5e109cb074e4ea8396ef0a8bd0f00e32f
Ian Denhardt <ian@zenhack.net>
2018-09-25 19:06:57 -0400

    Add examples directory, with echo client.
    The server is not yet included; I wrote a one-off thing in Go to test,
    but I plan to add a Haskell server as an example when the library can
    support it.

---
+++ examples/Main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Control.Exception      (bracket)
+import Control.Monad.IO.Class (liftIO)
+import Data.Function          ((&))
+import Network.Simple.TCP     (connectSock)
+import Network.Socket         (socketToHandle)
+import System.Exit            (exitSuccess)
+import System.IO              (IOMode(ReadWriteMode), hClose)
+
+import Data.Capnp        (def, defaultLimit)
+import Network.RPC.Capnp (bootstrap, handleTransport, runRpcT)
+
+import Capnp.Echo.Pure
+
+main = do
+    bracket openConn hClose talk
+  where
+    openConn = do
+        (sock, _addr) <- connectSock "localhost" "4000"
+        socketToHandle sock ReadWriteMode
+    talk handle = do
+        runRpcT def (handleTransport defaultLimit handle) $ do
+            echoSrv <- Echo <$> bootstrap
+            result <- echoSrv & echo'echo def { query = "Hello, World!" }
+            liftIO $ do
+                print result
+                exitSuccess
---
+++ examples/echo.capnp
@@ -0,0 +1,5 @@
+@0xd0a87f36fa0182f5;
+
+interface Echo {
+  echo @0 (query :Text) -> (reply :Text);
+}
