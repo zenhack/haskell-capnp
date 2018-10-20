7514800d3d6599b7350fb22be84601eca848c2d0
Ian Denhardt <ian@zenhack.net>
2018-10-19 22:37:17 -0400

    Move root modules re: the change in generated output.

--- lib/Capnp/Capnp.hs
+++
@@ -1,8 +0,0 @@
-{-|
-Module: Capnp.Capnp
-Description: Generated modules for the schema that ship with Cap'N Proto
-
-The modules under 'Capnp.Capnp' are generated code for the schema files that
-ship with the Cap'N Proto reference implementation.
--}
-module Capnp.Capnp () where
