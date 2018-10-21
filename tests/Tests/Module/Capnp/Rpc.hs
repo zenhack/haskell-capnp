146af79f1e9c4b84acf9b1cc92b4373fa81309ec
Ian Denhardt <ian@zenhack.net>
2018-10-21 17:10:10 -0400

    Add echo client & server to test suite.
    Per the comment, there's a bit of copypasta from the example, but I want
    to keep the example simple so it can server as documentation.

--- tests/Tests/Module/Capnp/Rpc.hs
+++ tests/Tests/Module/Capnp/Rpc.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Tests.Module.Capnp.Rpc (rpcTests) where
 
 import Test.Hspec
 
-import Control.Concurrent     (MVar, newEmptyMVar, putMVar, takeMVar)
-import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Concurrent       (MVar, newEmptyMVar, putMVar, takeMVar)
+import Control.Concurrent.Async (race_)
+import Control.Monad.IO.Class   (MonadIO, liftIO)
+import Data.Function            ((&))
 
-import Capnp     (ConstMsg)
+import Capnp     (ConstMsg, def)
 import Capnp.Rpc
+
+import Capnp.Gen.Echo.Pure
 
 -- | Make a pair of in-memory transports that are connected to one another. i.e,
 -- messages sent on one are received on the other.
@@ -29,4 +34,38 @@ mVarTransport sendVar recvVar = Transport
     }
 
 rpcTests :: Spec
-rpcTests = pure ()
+rpcTests = describe "Echo server & client" $
+    it "Should echo back the same message." $ do
+        -- XXX: this is copypasta from the examples dir. We should find
+        -- a way to share more of the code, while keeping the example
+        -- useful as documentation.
+        (clientTrans, serverTrans) <- transportPair
+        let runClient = runVat def
+                { debugMode = True }
+                clientTrans
+                $ do
+                    echoSrv <- Echo <$> bootstrap
+                    let msgs =
+                            [ def { query = "Hello #1" }
+                            , def { query = "Hello #2" }
+                            ]
+                    rets <- traverse (\msg -> echoSrv & echo'echo msg) msgs
+                    liftIO $ rets `shouldBe`
+                        [ def { reply = "Hello #1" }
+                        , def { reply = "Hello #2" }
+                        ]
+                    stopVat
+            runServer = runVat def
+                { debugMode = True
+                , bootstrapServer = Just $ do
+                    Echo client <- export_Echo TestEchoServer
+                    pure client
+                }
+                serverTrans
+                (pure ())
+        race_ runServer runClient
+
+data TestEchoServer = TestEchoServer
+
+instance Echo'server_ TestEchoServer where
+    echo'echo params TestEchoServer = pure def { reply = query params }
