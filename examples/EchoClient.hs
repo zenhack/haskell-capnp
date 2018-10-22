6a2f25cda3c58880366ab9e45f35b725c06bf701
Ian Denhardt <ian@zenhack.net>
2018-10-21 21:06:59 -0400

    Examples: remove a bunch of unused imports.
    Turned on -Wall in the cabal file; it caught this (mostly leftovers from
    older implementations), and the fact that main didn't have a type
    annotation (also fixed).

--- examples/EchoClient.hs
+++ examples/EchoClient.hs
@@ -3,17 +3,14 @@ module Main where
 
 import Control.Monad.IO.Class (liftIO)
 import Data.Function          ((&))
-import Network.Simple.TCP     (connect, serve)
-import System.Environment     (getArgs)
-import System.Exit            (exitFailure, exitSuccess)
-import System.IO              (IOMode(ReadWriteMode), hClose)
+import Network.Simple.TCP     (connect)
 
-import Capnp     (def, defaultLimit)
-import Capnp.Rpc
-    (VatConfig(..), bootstrap, runVat, socketTransport, stopVat, vatConfig)
+import Capnp     (def)
+import Capnp.Rpc (VatConfig(..), runVat, socketTransport, stopVat, vatConfig)
 
 import Capnp.Gen.Echo.Pure
 
+main :: IO ()
 main = connect "localhost" "4000" $ \(sock, _addr) -> do
     runVat $ (vatConfig $ socketTransport sock)
         { debugMode = True
--- examples/EchoServer.hs
+++ examples/EchoServer.hs
@@ -1,16 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Monad.IO.Class (liftIO)
-import Data.Function          ((&))
-import Network.Simple.TCP     (connect, serve)
-import System.Environment     (getArgs)
-import System.Exit            (exitFailure, exitSuccess)
-import System.IO              (IOMode(ReadWriteMode), hClose)
+import Network.Simple.TCP (serve)
 
-import Capnp     (def, defaultLimit)
-import Capnp.Rpc
-    (VatConfig(..), bootstrap, runVat, socketTransport, stopVat, vatConfig)
+import Capnp     (def)
+import Capnp.Rpc (VatConfig(..), runVat, socketTransport, vatConfig)
 
 import Capnp.Gen.Echo.Pure
 
@@ -19,6 +13,7 @@ data MyEchoServer = MyEchoServer
 instance Echo'server_ MyEchoServer where
     echo'echo params MyEchoServer = pure def { reply = query params }
 
+main :: IO ()
 main = serve "localhost" "4000" $ \(sock, _addr) -> do
     runVat $ (vatConfig $ socketTransport sock)
         { offerBootstrap  = Just $ do
