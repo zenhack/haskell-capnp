ee22670fd53bda13ce1a4b0e567d37b77e13ff7b
Ian Denhardt <ian@zenhack.net>
2018-10-22 22:43:13 -0400

    Update hlint script to point to the new cmd directory.

--- scripts/hlint.sh
+++ scripts/hlint.sh
@@ -5,7 +5,7 @@
 # We skip generated files, since we somtimes deliberately use conventions
 # in the output that hlint will flag, to make codegen easier.
 cd "$(dirname $0)/.."
-exec hlint $(find lib examples exe tests -name '*.hs' \
+exec hlint $(find lib examples cmd tests -name '*.hs' \
 		| grep -v examples/Capnp/Gen/ \
 		| grep -v lib/Capnp/Gen/ \
 		| grep -v lib/Internal/Gen)
