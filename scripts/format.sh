63bdfe7cf89b000243a0853576625c251f1b5bb8
Ian Denhardt <ian@zenhack.net>
2018-10-26 15:25:53 -0400

    Update ignore paths re: removing internal modules.

--- scripts/format.sh
+++ scripts/format.sh
@@ -5,5 +5,6 @@ set -e
 cd "$(dirname $0)/.."
 stylish-haskell -i $(find * -name '*.hs' \
 	| grep -v examples/Capnp/Gen/ \
+	| grep -v tests/main/Capnp/Gen/ \
 	| grep -v lib/Capnp/Gen/ \
 	| grep -v lib/Internal/Gen)
