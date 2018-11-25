f5bf3b729b5a8a1fedcc4fd682f26fe7b7be5706
Ian Denhardt <ian@zenhack.net>
2018-11-25 17:30:41 -0500

    Get the calculator server example working.
    The last bit was actually bug in our implementation of the example, not
    the RPC system.

--- examples/CalculatorServer.hs
+++ examples/CalculatorServer.hs
@@ -101,8 +101,8 @@ eval args (Expression'parameter idx)
         throwFailed "Parameter index out of bounds"
     | otherwise =
         pure $ args V.! fromIntegral idx
-eval _ Expression'call{function, params} = do
-    args' <- traverse (eval V.empty) params
+eval outerParams Expression'call{function, params=innerParams} = do
+    args' <- traverse (eval outerParams) innerParams
     Function'call'results{value} <-
         function'call function ? Function'call'params { params = args' }
         >>= waitIO
