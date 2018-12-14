94778d803d0fb3528c28522cd6ebfd85d80dec4f
Ian Denhardt <ian@zenhack.net>
2018-12-13 20:14:29 -0500

    Don't spawn a separate process when testing the calculator server.
    This way the coverage tool will see the parts of the server that we're
    running.
    
    This also fixes a build error, since the call to new-run would have had
    to change.

--- tests/CalculatorExample.hs
+++ tests/CalculatorExample.hs
@@ -4,12 +4,15 @@ module CalculatorExample (tests) where
 
 import Test.Hspec
 
-import Control.Concurrent (threadDelay)
-import Control.Exception  (SomeException, try)
-import Data.Foldable      (for_)
-import System.Environment (getEnv)
-import System.Exit        (ExitCode(ExitSuccess))
-import System.Process     (proc, readProcessWithExitCode, withCreateProcess)
+import Control.Concurrent       (threadDelay)
+import Control.Concurrent.Async (race_)
+import Control.Exception        (SomeException, try)
+import Data.Foldable            (for_)
+import System.Environment       (getEnv)
+import System.Exit              (ExitCode(ExitSuccess))
+import System.Process           (readProcessWithExitCode)
+
+import qualified Examples.CalculatorServer
 
 tests :: Spec
 tests = describe "Check our example against the C++ implementation" $ do
@@ -22,22 +25,24 @@ tests = describe "Check our example against the C++ implementation" $ do
             Right path ->
                 pure (Just path)
     for_ cxxPath $ \path ->
-        it "Should pass when run against our server" $ do
-            (eStatus, out, err)  <- withCreateProcess
-                    (proc "cabal" ["new-run", "calculator-server"]) $
-                \_ _ _ _ -> do
-                    -- new-run takes a moment to start up:
+        it "Should pass when run against our server" $
+            race_
+                Examples.CalculatorServer.main
+                (do
+                    -- Give the server plenty of time to start up:
                     threadDelay 2000000
-                    readProcessWithExitCode path ["localhost:4000"] ""
-            (eStatus, out, err)
-                `shouldBe`
-                ( ExitSuccess
-                , unlines
-                    [ "Evaluating a literal... PASS"
-                    , "Using add and subtract... PASS"
-                    , "Pipelining eval() calls... PASS"
-                    , "Defining functions... PASS"
-                    , "Using a callback... PASS"
-                    ]
-                , ""
+
+                    (eStatus, out, err) <- readProcessWithExitCode path ["localhost:4000"] ""
+                    (eStatus, out, err)
+                        `shouldBe`
+                        ( ExitSuccess
+                        , unlines
+                            [ "Evaluating a literal... PASS"
+                            , "Using add and subtract... PASS"
+                            , "Pipelining eval() calls... PASS"
+                            , "Defining functions... PASS"
+                            , "Using a callback... PASS"
+                            ]
+                        , ""
+                        )
                 )
