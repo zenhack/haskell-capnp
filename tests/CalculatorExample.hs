b9f883e2294cc8e6b67f7e4d76d3b888aed99a89
Ian Denhardt <ian@zenhack.net>
2018-12-14 20:03:18 -0500

    Add a test proxying the c++ calculator client & server.
    Fails, predictably.

--- tests/CalculatorExample.hs
+++ tests/CalculatorExample.hs
@@ -1,22 +1,28 @@
 {-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module CalculatorExample (tests) where
 
+import Data.Word
 import Test.Hspec
 
 import Control.Concurrent       (threadDelay)
 import Control.Concurrent.Async (race_)
 import Control.Exception        (throwIO, try)
+import Data.Default             (def)
 import Data.Foldable            (for_)
+import Data.String              (fromString)
+import Network.Simple.TCP       (connect, serve)
 import System.Environment       (getEnv)
 import System.Exit              (ExitCode(ExitSuccess))
 import System.IO.Error          (isDoesNotExistError)
-import System.Process           (readProcessWithExitCode)
+import System.Process           (callProcess, readProcessWithExitCode)
+
+import Capnp.Rpc.Transport  (socketTransport)
+import Capnp.Rpc.Untyped    (ConnConfig(..), handleConn)
+import Capnp.TraversalLimit (defaultLimit)
 
 import qualified Examples.CalculatorServer
-
-getCxxClient :: IO (Maybe FilePath)
-getCxxClient = getExe "CXX_CALCULATOR_CLIENT"
 
 getExe :: String -> IO (Maybe FilePath)
 getExe varName =
@@ -32,26 +38,55 @@ getExe varName =
 
 tests :: Spec
 tests = describe "Check our example against the C++ implementation" $ do
-    cxxPath <- runIO $ getCxxClient
-    for_ cxxPath $ \path ->
+    clientPath <- runIO $ getExe "CXX_CALCULATOR_CLIENT"
+    serverPath <- runIO $ getExe "CXX_CALCULATOR_SERVER"
+    for_ clientPath $ \clientPath ->
         it "Should pass when run against our server" $
-            race_
-                Examples.CalculatorServer.main
-                (do
-                    -- Give the server plenty of time to start up:
-                    threadDelay 2000000
+            Examples.CalculatorServer.main
+                `race_` (waitForServer >> cxxClient clientPath 4000)
+    for_ ((,) <$> clientPath <*> serverPath) $ \(clientPath, serverPath) ->
+        it "Should pass when run aginst the C++ server, proxied through us." $
+            cxxServer serverPath 4000
+                `race_` (waitForServer >> runProxy 4000 6000)
+                -- we wait twice, so that the proxy also has time to start:
+                `race_` (waitForServer >> waitForServer >> cxxClient clientPath 6000)
+  where
+    -- | Give the server a bit of time to start up.
+    waitForServer :: IO ()
+    waitForServer = threadDelay 1000000
 
-                    (eStatus, out, err) <- readProcessWithExitCode path ["localhost:4000"] ""
-                    (eStatus, out, err)
-                        `shouldBe`
-                        ( ExitSuccess
-                        , unlines
-                            [ "Evaluating a literal... PASS"
-                            , "Using add and subtract... PASS"
-                            , "Pipelining eval() calls... PASS"
-                            , "Defining functions... PASS"
-                            , "Using a callback... PASS"
-                            ]
-                        , ""
-                        )
-                )
+    cxxServer :: FilePath -> Word16 -> IO ()
+    cxxServer path port =
+        callProcess path ["localhost:" ++ show port ]
+    cxxClient :: FilePath -> Word16 -> IO ()
+    cxxClient path port = do
+        (eStatus, out, err) <- readProcessWithExitCode path ["localhost:" ++ show port] ""
+        (eStatus, out, err)
+            `shouldBe`
+            ( ExitSuccess
+            , unlines
+                [ "Evaluating a literal... PASS"
+                , "Using add and subtract... PASS"
+                , "Pipelining eval() calls... PASS"
+                , "Defining functions... PASS"
+                , "Using a callback... PASS"
+                ]
+            , ""
+            )
+
+-- | @'runProxy' serverPort clientPort@ connects to the server listening at
+-- localhost:serverPort, requests its bootstrap interface, and then listens
+-- on clientPort, offering a proxy of the server's bootstrap interface as our
+-- own.
+runProxy :: Word16 -> Word16 -> IO ()
+runProxy serverPort clientPort = do
+    connect "localhost" (fromString $ show serverPort) $ \(serverSock, _addr) ->
+        handleConn (socketTransport serverSock defaultLimit) def
+            { debugMode = True
+            , withBootstrap = Just $ \_sup client -> do
+                serve "localhost" (fromString $ show clientPort) $ \(clientSock, _addr) ->
+                    handleConn (socketTransport clientSock defaultLimit) def
+                        { getBootstrap = \_sup -> pure $ Just client
+                        , debugMode = True
+                        }
+            }
