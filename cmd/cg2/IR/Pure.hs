966c217f7c40f080c6e986f0cee5c23d246f5392
Ian Denhardt <ian@zenhack.net>
2018-12-25 00:19:18 -0500

    Sketch a bit of the pure IR.

--- cmd/cg2/IR/Pure.hs
+++ cmd/cg2/IR/Pure.hs
@@ -1,1 +1,31 @@
 module IR.Pure where
+
+import qualified IR.Name as Name
+
+data Decl
+    = DUnion
+        { typeName :: Name.LocalQ
+        , variants :: [Variant]
+        }
+    | DStruct
+        { typeName :: Name.LocalQ
+        , fields   :: [Field]
+        }
+
+data Field = Field
+    { name     :: Name.UnQ
+    -- ^ The name of the field.
+
+    , cerialEq :: !Bool
+    -- ^ Whether the serialized and unserialized forms of this type
+    -- are the same. If not, there is a marshalling step, if so, the
+    -- accessors work with the decerialized form directly.
+    --
+    -- For example, this is True for Enums, basic integers, etc. but
+    -- False for structs, interfaces, etc.
+    }
+
+data Variant = Variant
+    { name   :: Name.LocalQ
+    , fields :: [Field]
+    }
