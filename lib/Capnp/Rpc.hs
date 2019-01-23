f26c3a1e91f300f1ad02d87ad19bb5e7a54323a5
Ian Denhardt <ian@zenhack.net>
2019-01-23 18:36:41 -0500

    Re-export a bit more transport stuff from Capnp.Rpc

--- lib/Capnp/Rpc.hs
+++ lib/Capnp/Rpc.hs
@@ -11,12 +11,14 @@ module Capnp.Rpc
     , throwFailed
 
     -- * Transmitting messages
-    , Transport
+    , Transport(..)
     , socketTransport
     , handleTransport
+    , tracingTransport
     ) where
 
 import Capnp.Rpc.Errors    (throwFailed)
 import Capnp.Rpc.Invoke    ((?))
-import Capnp.Rpc.Transport (Transport, handleTransport, socketTransport)
+import Capnp.Rpc.Transport
+    (Transport(..), handleTransport, socketTransport, tracingTransport)
 import Capnp.Rpc.Untyped   (ConnConfig(..), handleConn)
