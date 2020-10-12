2956424c87530873ee99c610ccd36ca3e59c6d25
Ian Denhardt <ian@zenhack.net>
2020-10-12 04:50:37 -0400

    Simplify solution to #74
    Per discussion in #76, it suffices to put the cell data behind a TVar,
    as this adds it to the transaction log, ensuring both the value and the
    finalizers are reachable at least until the end of the transaction.

--- lib/Internal/Finalizer.hs
+++ lib/Internal/Finalizer.hs
@@ -23,9 +23,11 @@ From the docs for the 'Weak' type:
 So instead, we provide a 'Cell' type, which:
 
 * Wraps simple value
-* Can be created inside STM, and
+* Can be created and read inside STM, and
 * May safely have finalizers, using the 'addFinalizer' function in
   this module.
+* Ensures that the finalizers will not be run before any transaction that
+  reads data is complete.
 
 Note that it is *not* safe to use the primitives from "Sys.Mem.Weak" to
 add finalizers.
@@ -36,80 +38,41 @@ module Internal.Finalizer (Cell, get, newCell, addFinalizer) where
 
 import Control.Concurrent.MVar (MVar, mkWeakMVar, newEmptyMVar)
 import Control.Concurrent.STM
-    ( STM
-    , TVar
-    , atomically
-    , modifyTVar'
-    , newTVar
-    , readTVar
-    , readTVarIO
-    , retry
-    , writeTVar
-    )
-import Control.Monad           (when)
-import GHC.Conc                (unsafeIOToSTM)
+    (STM, TVar, atomically, modifyTVar', newTVar, readTVar)
 
 -- | A cell, containing a value and possibly finalizers.
-newtype Cell a = Cell (TVar (CellState a))
+newtype Cell a
+    = Cell (TVar (CellData a))
     deriving(Eq)
 
-data CellState a = CellState
-    { value       :: a
+-- The actual contents of a cell. This is wrapped in a 'TVar' to force accesses
+-- to add the a reference the transaction log from which the finalizers are
+-- reachable, thus preventing them from running before the completion of any
+-- transaction that examines the value.
+data CellData a = CellData
+    { value      :: a
     -- ^ The value wrapped by the cell.
-    , finalizers  :: TVar [MVar ()]
+
+    , finalizers :: [MVar ()]
     -- ^ Experimentally, TVars appear not to be safe for finalizers, so
     -- instead we create MVars for the finalizers, and store them this
     -- list so that we maintain a reference to them.
-
-    , isFinalized :: TVar Bool
-    -- ^ Note [Finalizer race]
     }
     deriving(Eq)
 
 -- | Get the value from a cell
 get :: Cell a -> STM a
-get (Cell state) = do
-    CellState{value, isFinalized} <- readTVar state
-    finalized <- readTVar isFinalized
-    when finalized $ do
-        -- Note [Finalizer race]
-        unsafeIOToSTM $ putStrLn "BUG in haskell-capnp: finalizer ran before Cell was unreachable!"
-        retry
-    pure value
+get (Cell state) = value <$> readTVar state
 
 -- Create  a new cell, initially with no finalizers.
 newCell :: a -> STM (Cell a)
-newCell value = do
-    isFinalized <- newTVar False
-    finalizers <- newTVar []
-    Cell <$> newTVar CellState { value, finalizers, isFinalized }
+newCell value = Cell <$> newTVar CellData { value, finalizers = [] }
 
 -- Add a new finalizer to the cell. Cells may have many finalizers
 -- attached.
 addFinalizer :: Cell a -> STM () -> IO ()
-addFinalizer (Cell state) fin = do
-    CellState {finalizers, isFinalized} <- readTVarIO state
+addFinalizer (Cell stateVar) fin = do
     mvar <- newEmptyMVar
-    _ <- mkWeakMVar mvar $ atomically $ do
-        fin
-        writeTVar isFinalized True
-    atomically $ modifyTVar' finalizers (mvar:)
-
--- Note [Finalizer race]
---
--- Per issue #74, in a previous implementation it was possible for the cell to
--- be collected before a transaction that used it completes, and for the finalizer
--- to run to completion also before the transaction completes. To avoid this,
--- we need to make sure that the finalizer and any transaction that uses the cell
--- conflict, such that the finalizer always comes in a *later* transaction.
---
--- To achieve this we store a shared isFinalized TVar, and write to it from the
--- finalizers. 'get' reads it and verifies that it has not been set; if it has
--- it retries.
---
--- Subtly, this actually prevents the finalizer from running, because the
--- possibility of a retry means we need to keep holding the reference so we
--- can access the cell in a subsequent attempt -- so the retry should never actually
--- occur, but still needs to be there for correctness. We add a print statement
--- to the branch, so that if there is a bug where this actually *can* happen,
--- we get some indicator of this, rather than just deadlocking.
+    _ <- mkWeakMVar mvar $ atomically fin
+    atomically $ modifyTVar' stateVar $ \state@CellData{finalizers} ->
+        state { finalizers = mvar : finalizers }
