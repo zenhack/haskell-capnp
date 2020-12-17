436ad67784eae63f26d192a6e3dd1208ca658d5f
Ian Denhardt <ian@zenhack.net>
2020-12-17 18:56:31 -0500

    Convert Codec.Capnp to Mutability

--- lib/Codec/Capnp.hs
+++ lib/Codec/Capnp.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies          #-}
@@ -9,22 +10,24 @@ module Codec.Capnp
 
 import Capnp.Classes
 
+import Capnp.Message (Mutability (..))
+
 import qualified Capnp.Message as M
 import qualified Capnp.Untyped as U
 
 -- | 'newRoot' allocates and returns a new value inside the message, setting
 -- it as the root object of the message.
-newRoot :: (ToStruct (M.MutMsg s) a, Allocate s a, M.WriteCtx m s)
-    => M.MutMsg s -> m a
+newRoot :: (ToStruct ('Mut s) a, Allocate s a, M.WriteCtx m s)
+    => M.Message ('Mut s) -> m a
 newRoot msg = do
     ret <- new msg
     setRoot ret
     pure ret
 
 -- | 'setRoot' sets its argument to be the root object in its message.
-setRoot :: (ToStruct (M.MutMsg s) a, M.WriteCtx m s) => a -> m ()
+setRoot :: (ToStruct ('Mut s) a, M.WriteCtx m s) => a -> m ()
 setRoot = U.setRoot . toStruct
 
 -- | 'getRoot' returns the root object of a message.
-getRoot :: (FromStruct msg a, U.ReadCtx m msg) => msg -> m a
+getRoot :: (FromStruct mut a, U.ReadCtx m mut) => M.Message mut -> m a
 getRoot msg = U.rootPtr msg >>= fromStruct
