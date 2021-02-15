6a993563318a7e862b670c9ce142b9ff408dc744
Ian Denhardt <ian@zenhack.net>
2021-02-15 17:09:16 -0500

    Experiment with repr based pipeline API

--- lib/Capnp/Repr/Methods.hs
+++ lib/Capnp/Repr/Methods.hs
@@ -7,11 +7,19 @@
 module Capnp.Repr.Methods
     ( Method(..)
     , HasMethod
+
+    , Pipeline(..)
+    , Client(..)
+    , pipe
+    , pipelineClient
     ) where
 
-import qualified Capnp.Repr           as R
+import qualified Capnp.Fields            as F
+import qualified Capnp.Repr              as R
+import qualified Capnp.Rpc.Untyped       as Rpc
+import           Control.Monad.STM.Class (MonadSTM(..))
 import           Data.Word
-import           GHC.OverloadedLabels (IsLabel)
+import           GHC.OverloadedLabels    (IsLabel)
 
 -- | Represents a method on the interface type @c@ with parameter
 -- type @p@ and return type @r@.
@@ -28,3 +36,23 @@ class
     ( R.ReprFor c ~ 'R.Ptr ('Just 'R.Cap)
     , IsLabel name (Method c p r)
     ) => HasMethod name c p r | name c -> p r
+
+newtype Pipeline a = Pipeline Rpc.Pipeline
+
+newtype Client a = Client Rpc.Client
+
+pipe :: ( R.ReprFor a ~ 'R.Ptr ('Just 'R.Struct)
+        , R.ReprFor b ~ 'R.Ptr pr
+        ) => F.Field a b -> Pipeline a -> Pipeline b
+pipe (F.Field field) (Pipeline p) =
+    case field of
+        F.GroupField   -> Pipeline p
+        F.PtrField idx -> Pipeline (Rpc.walkPipelinePtr p idx)
+
+pipelineClient :: (R.ReprFor a ~ 'R.Ptr ('Just 'R.Cap), MonadSTM m) => Pipeline a -> m (Client a)
+pipelineClient (Pipeline p) =
+    liftSTM $ Client <$> Rpc.pipelineClient p
+
+instance R.ReprFor a ~ 'R.Ptr ('Just 'R.Cap) => Rpc.IsClient (Client a) where
+    toClient (Client c) = c
+    fromClient = Client
