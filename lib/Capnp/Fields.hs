56693c2d3e50550b8c9dc65e36d231d593d10b54
Ian Denhardt <ian@zenhack.net>
2021-02-15 16:02:01 -0500

    Comments in Capnp.Fields

--- lib/Capnp/Fields.hs
+++ lib/Capnp/Fields.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GADTs                  #-}
 {-# LANGUAGE KindSignatures         #-}
+-- | Module: Capnp.Fields
+-- Description: Support for working with struct fields
 module Capnp.Fields
     ( HasField
     , Field(..)
@@ -17,12 +19,18 @@ import GHC.OverloadedLabels (IsLabel)
 import qualified Capnp.Classes as C
 import qualified Capnp.Repr    as R
 
+-- | @'Field' a b@ is a first-class representation of a field of type @b@ within
+-- an @a@, where @a@ must be a struct type.
+newtype Field a b = Field (FieldLoc (R.ReprFor b))
+
+-- | The location of a field within a message.
 data FieldLoc (r :: R.Repr) where
     GroupField :: FieldLoc ('R.Ptr ('Just 'R.Struct))
     PtrField :: R.IsPtrRepr a => Word16 -> FieldLoc ('R.Ptr a)
     DataField :: C.IsWord (R.UntypedData a) => DataFieldLoc a -> FieldLoc ('R.Data a)
     VoidField :: FieldLoc ('R.Data 'R.Sz0)
 
+-- | The location of a data (non-pointer) field.
 data DataFieldLoc (sz :: R.DataSz) = DataFieldLoc
     { shift        :: !BitCount
     , index        :: !Word16
@@ -30,8 +38,9 @@ data DataFieldLoc (sz :: R.DataSz) = DataFieldLoc
     , defaultValue :: !Word64
     }
 
-newtype Field a b = Field (FieldLoc (R.ReprFor b))
-
+-- | An instance @'HasField' name a b@ indicates that the struct type @a@
+-- has a field named @name@ with type @b@. The generated code includes
+-- instances of this for each field in the schema.
 class
     ( R.ReprFor a ~ 'R.Ptr ('Just 'R.Struct)
     , IsLabel name (Field a b)
