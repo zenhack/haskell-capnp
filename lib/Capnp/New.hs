376cb3b0f7bbf9e13608e6e49669db49c20a2d8c
Ian Denhardt <ian@zenhack.net>
2021-07-29 22:54:32 -0400

    Export a few more things from Capnp.New

--- lib/Capnp/New.hs
+++ lib/Capnp/New.hs
@@ -1,11 +1,16 @@
 module Capnp.New
     ( module X
     , Parsed
+    , def
     ) where
 
+import Capnp.Convert         as X
 import Capnp.Fields          as X
+import Capnp.IO              as X
 import Capnp.New.Accessors   as X
 import Capnp.New.Classes     as X hiding (Parsed)
 import Capnp.New.Constraints as X
 import Capnp.Repr.Methods    as X
 import Capnp.Repr.Parsed     (Parsed)
+
+import Data.Default (def)
