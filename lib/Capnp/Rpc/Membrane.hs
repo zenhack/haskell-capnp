77abd8e3711d0d0e715582e986085d6159a0968c
Ian Denhardt <ian@zenhack.net>
2023-01-04 17:35:33 -0500

    Document Membrane module

--- lib/Capnp/Rpc/Membrane.hs
+++ lib/Capnp/Rpc/Membrane.hs
@@ -2,13 +2,22 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 
+-- | Module: Capnp.Rpc.Membrane
+-- Descritpion: Helpers for working with membranes.
+--
+-- Membranes are common in object-capability design. Think of it like a
+-- proxy on steroids: a membrane inserts itself in front of another capability,
+-- and can intercept and modify method calls. Unlike a simple proxy though,
+-- the membrane will also be applied to any objects returned by method calls,
+-- or passed in arguments, transitively, so it can sit in front of entire
+-- object graphs.
 module Capnp.Rpc.Membrane
-  ( Action (..),
+  ( enclose,
+    exclude,
+    Policy,
+    Action (..),
     Direction (..),
     Call (..),
-    Policy,
-    enclose,
-    exclude,
   )
 where
 
@@ -25,10 +34,19 @@ import Data.Typeable (Typeable, cast)
 import Data.Word
 import Supervisors (Supervisor)
 
+-- | An action indicates what to do with an incoming method call.
 data Action
-  = Handle Server.UntypedMethodHandler
-  | Forward
+  = -- | Handle the method using the provided method handler, instead of
+    -- letting it through the membrane. Arguments and return values will not
+    -- be wrapped/unwraped, so be careful when delegating to objects inside
+    -- the membrane.
+    Handle Server.UntypedMethodHandler
+  | -- | Forward the method call on to its original destination, wrapping
+    -- and unwrapping arguments & return values as normal.
+    Forward
 
+-- | A Direction indicates which direction a method call is traveling:
+-- into or out of the membrane.
 data Direction = In | Out
   deriving (Show, Read, Eq)
 
@@ -36,20 +54,36 @@ flipDir :: Direction -> Direction
 flipDir In = Out
 flipDir Out = In
 
+-- | Alias for direction; somtimes it is convienent to think about capabilities
+-- from the standpoint of which side they is _on_, rather than where it is going
+-- as with methods.
 type Side = Direction
 
+-- | A 'Call' represents a method call that is crossing the membrane.
 data Call = Call
-  { direction :: Direction,
+  { -- | Which direction is the call going? if this is 'In', the call was made
+    -- by something outside the membrane to something inside it. If it is 'Out',
+    -- something inside the membrane is making a call to something outside the
+    -- membrane.
+    direction :: Direction,
+    -- | The interface id of the method being called.
     interfaceId :: Word64,
+    -- | The ordinal of the method being called.
     methodId :: Word16,
+    -- | The target of the method call.
     target :: URpc.Client
   }
 
+-- | A 'Policy' decides what to do when a call crosses the membrane.
 type Policy = Call -> STM Action
 
+-- | @'enclose' sup cap policy@ wraps @cap@ in a membrane whose behavior is
+-- goverend by @policy@.
 enclose :: (URpc.IsClient c, MonadSTM m) => Supervisor -> c -> Policy -> m c
 enclose = newMembrane In
 
+-- | 'exclude' is like 'enclose', except that the capability is treated as
+-- being *outside* of a membrane that wraps the rest of the world.
 exclude :: (URpc.IsClient c, MonadSTM m) => Supervisor -> c -> Policy -> m c
 exclude = newMembrane Out
 
