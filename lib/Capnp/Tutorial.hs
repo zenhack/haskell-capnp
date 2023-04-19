edb226753186749558d361fbc4e9a747b3777afb
Ian Denhardt <ian@zenhack.net>
2023-04-18 20:21:46 -0400

    Update Tutorial with latest examples

--- lib/Capnp/Tutorial.hs
+++ lib/Capnp/Tutorial.hs
@@ -267,50 +267,53 @@ import System.IO (stdout)
 -- functionality from the capnp package. We can write an address book
 -- message to standard output using the high-level API like so:
 --
--- > {-# LANGUAGE OverloadedStrings     #-}
 -- > -- Note that DuplicateRecordFields is usually needed, as the generated
 -- > -- code relys on it to resolve collisions in capnproto struct field
 -- > -- names:
 -- > {-# LANGUAGE DuplicateRecordFields #-}
--- > import Capnp.Gen.Addressbook
+-- > {-# LANGUAGE OverloadedStrings #-}
+-- >
+-- > module Examples.Serialization.HighLevel.Write (main) where
 -- >
 -- > -- Note that Capnp re-exports `def`, as a convienence
--- > import Capnp (putParsed, def)
+-- > import Capnp (def, putParsed)
+-- > import Capnp.Gen.Addressbook
 -- >
--- > import qualified Data.Vector as V
--- >
--- > main = putParsed AddressBook
--- >     { people = V.fromList
--- >         [ Person
--- >             { id = 123
--- >             , name = "Alice"
--- >             , email = "alice@example.com"
--- >             , phones = V.fromList
--- >                 [ def
--- >                     { number = "555-1212"
--- >                     , type_ =  Person'PhoneNumber'Type'mobile
--- >                     }
--- >                 ]
--- >             , employment = Person'employment $ Person'employment'school "MIT"
--- >             }
--- >         , Person
--- >             { id = 456
--- >             , name = "Bob"
--- >             , email = "bob@example.com"
--- >             , phones = V.fromList
--- >                 [ def
--- >                     { number = "555-4567"
--- >                     , type_ = Person'PhoneNumber'Type'home
--- >                     }
--- >                 , def
--- >                     { number = "555-7654"
--- >                     , type_ = Person'PhoneNumber'Type'work
--- >                     }
--- >                 ]
--- >             , employment = Person'employment $ Person'employment'selfEmployed
--- >             }
--- >         ]
--- >     }
+-- > main :: IO ()
+-- > main =
+-- >   putParsed
+-- >     AddressBook
+-- >       { people =
+-- >           [ Person
+-- >               { id = 123,
+-- >                 name = "Alice",
+-- >                 email = "alice@example.com",
+-- >                 phones =
+-- >                   [ def
+-- >                       { number = "555-1212",
+-- >                         type_ = Person'PhoneNumber'Type'mobile
+-- >                       }
+-- >                   ],
+-- >                 employment = Person'employment' $ Person'employment'school "MIT"
+-- >               },
+-- >             Person
+-- >               { id = 456,
+-- >                 name = "Bob",
+-- >                 email = "bob@example.com",
+-- >                 phones =
+-- >                   [ def
+-- >                       { number = "555-4567",
+-- >                         type_ = Person'PhoneNumber'Type'home
+-- >                       },
+-- >                     def
+-- >                       { number = "555-7654",
+-- >                         type_ = Person'PhoneNumber'Type'work
+-- >                       }
+-- >                   ],
+-- >                 employment = Person'employment' Person'employment'selfEmployed
+-- >               }
+-- >           ]
+-- >       }
 --
 -- 'putValue' is equivalent to @'hPutValue' 'stdout'@; 'hPutValue' may be used
 -- to write to an arbitrary handle.
@@ -382,112 +385,113 @@ import System.IO (stdout)
 -- > {-# LANGUAGE OverloadedLabels #-}
 -- > {-# LANGUAGE TypeApplications #-}
 -- >
--- > import           Capnp.Gen.Addressbook
--- > import qualified Capnp                 as C
--- > import           Control.Monad             (forM_)
--- > import           Control.Monad.Trans       (lift)
--- > import           Data.Function             ((&))
--- > import qualified Data.Text                 as T
+-- > module Examples.Serialization.LowLevel.Read (main) where
+-- >
+-- > import Capnp.Gen.Addressbook
+-- > import qualified Capnp as C
+-- > import Control.Monad (forM_)
+-- > import Control.Monad.Trans (lift)
+-- > import Data.Function ((&))
+-- > import qualified Data.Text as T
 -- >
 -- > main :: IO ()
 -- > main = do
--- >     addressbook <- C.getRaw @AddressBook C.defaultLimit
--- >     C.evalLimitT C.defaultLimit $ do
--- >         people <- C.readField #people addressbook
--- >         forM_ [0..C.length people - 1] $ \i -> do
--- >             people
--- >                 & C.index i
--- >                 >>= C.parseField #name
--- >                 >>= lift . putStrLn . T.unpack
+-- >   addressbook <- C.getRaw @AddressBook C.defaultLimit
+-- >   C.evalLimitT C.defaultLimit $ do
+-- >     people <- C.readField #people addressbook
+-- >     forM_ [0 .. C.length people - 1] $ \i -> do
+-- >       people
+-- >         & C.index i
+-- >         >>= C.parseField #name
+-- >         >>= lift . putStrLn . T.unpack
 
 -- $lowlevel-write
 --
 -- Writing messages using the low-level API has a similarly imperative feel.
 -- The below constructs the same message as in our high-level example above:
 --
--- > {-# LANGUAGE DataKinds        #-}
+-- > {-# LANGUAGE DataKinds #-}
 -- > {-# LANGUAGE OverloadedLabels #-}
 -- > {-# LANGUAGE TypeApplications #-}
 -- >
--- > import Data.Function ((&))
+-- > module Examples.Serialization.LowLevel.Write (main) where
 -- >
 -- > import Capnp.Gen.Addressbook
--- >
 -- > import qualified Capnp as C
+-- > import Data.Function ((&))
 -- > import qualified Data.Text as T
 -- >
 -- > main :: IO ()
 -- > main =
--- >     let Right msg = C.createPure C.defaultLimit buildMsg
--- >     in C.putMsg msg
+-- >   let Right msg = C.createPure C.defaultLimit buildMsg
+-- >    in C.putMsg msg
 -- >
 -- > buildMsg :: C.PureBuilder s (C.Message ('C.Mut s))
 -- > buildMsg = do
--- >     -- newMessage allocates a new, initially empty, mutable message. It
--- >     -- takes an optional size hint:
--- >     msg <- C.newMessage Nothing
+-- >   -- newMessage allocates a new, initially empty, mutable message. It
+-- >   -- takes an optional size hint:
+-- >   msg <- C.newMessage Nothing
 -- >
--- >     -- newRoot allocates a new struct as the root object of the message.
--- >     -- The unit argument is a hint to the allocator to determine the size
--- >     -- of the object; for types whose size is not fixed (e.g. untyped structs,
--- >     -- lists), this may be something more meaningful.
--- >     addressbook <- C.newRoot @AddressBook () msg
+-- >   -- newRoot allocates a new struct as the root object of the message.
+-- >   -- The unit argument is a hint to the allocator to determine the size
+-- >   -- of the object; for types whose size is not fixed (e.g. untyped structs,
+-- >   -- lists), this may be something more meaningful.
+-- >   addressbook <- C.newRoot @AddressBook () msg
 -- >
--- >     -- newField can be used to allocate the value of a field, for pointer
--- >     -- types like lists. The number is the allocation hint, as used by newRoot.
--- >     -- We can use the OverloadedLabels extension to pass in fields by name.
--- >     people <- C.newField #people 2 addressbook
+-- >   -- newField can be used to allocate the value of a field, for pointer
+-- >   -- types like lists. The number is the allocation hint, as used by newRoot.
+-- >   -- We can use the OverloadedLabels extension to pass in fields by name.
+-- >   people <- C.newField #people 2 addressbook
 -- >
--- >     -- Index gets an object at a specified location in a list. Cap'N Proto
--- >     -- lists are flat arrays, and in the case of structs the structs are
--- >     -- unboxed, so there is no need to allocate each element:
--- >     alice <- C.index 0 people
+-- >   -- Index gets an object at a specified location in a list. Cap'N Proto
+-- >   -- lists are flat arrays, and in the case of structs the structs are
+-- >   -- unboxed, so there is no need to allocate each element:
+-- >   alice <- C.index 0 people
 -- >
--- >     -- encodeField takes the parsed form of a value and marshals it into
--- >     -- the specified field. For basic types like integers & booleans, this
--- >     -- is almost always what you want. For larger values, you may want to
--- >     -- use newField as above, or separately create the value and use setField,
--- >     -- as shown below.
--- >     C.encodeField #id 123 alice
--- >     C.encodeField #name (T.pack "Alice") alice
--- >     C.encodeField #email (T.pack "alice@example.com") alice
+-- >   -- encodeField takes the parsed form of a value and marshals it into
+-- >   -- the specified field. For basic types like integers & booleans, this
+-- >   -- is almost always what you want. For larger values, you may want to
+-- >   -- use newField as above, or separately create the value and use setField,
+-- >   -- as shown below.
+-- >   C.encodeField #id 123 alice
+-- >   C.encodeField #name (T.pack "Alice") alice
+-- >   C.encodeField #email (T.pack "alice@example.com") alice
 -- >
--- >     -- We would probably use newField here, but to demonstrate, we can allocate
--- >     -- the value separately with new, and then set it with setField.
--- >     phones <- C.new @(C.List Person'PhoneNumber) 1 msg
--- >     C.setField #phones phones alice
+-- >   -- We would probably use newField here, but to demonstrate, we can allocate
+-- >   -- the value separately with new, and then set it with setField.
+-- >   phones <- C.new @(C.List Person'PhoneNumber) 1 msg
+-- >   C.setField #phones phones alice
 -- >
--- >     mobilePhone <- C.index 0 phones
--- >     -- It is sometimes more ergonomic to use (&) from Data.Function. You might
--- >     -- ask why not just make the container the first argument, but it works
--- >     -- out better this way for the read examples.
--- >     mobilePhone & C.encodeField #number (T.pack "555-1212")
--- >     mobilePhone & C.encodeField #type_ Person'PhoneNumber'Type'mobile
+-- >   mobilePhone <- C.index 0 phones
+-- >   -- It is sometimes more ergonomic to use (&) from Data.Function. You might
+-- >   -- ask why not just make the container the first argument, but it works
+-- >   -- out better this way for the read examples.
+-- >   mobilePhone & C.encodeField #number (T.pack "555-1212")
+-- >   mobilePhone & C.encodeField #type_ Person'PhoneNumber'Type'mobile
 -- >
--- >     -- Since named unions act like unnamed unions inside a group, we first have
--- >     -- to get the group field:
--- >     employment <- C.readField #employment alice
+-- >   -- Since named unions act like unnamed unions inside a group, we first have
+-- >   -- to get the group field:
+-- >   employment <- C.readField #employment alice
 -- >
--- >     -- Then, we can use encodeVariant to set both the tag of the union and the
--- >     -- value:
--- >     employment & C.encodeVariant #school (T.pack "MIT")
+-- >   -- Then, we can use encodeVariant to set both the tag of the union and the
+-- >   -- value:
+-- >   employment & C.encodeVariant #school (T.pack "MIT")
 -- >
--- >     bob <- C.index 1 people
--- >     bob & C.encodeField #id 456
--- >     bob & C.encodeField #name (T.pack "Bob")
--- >     bob & C.encodeField #email (T.pack "bob@example.com")
+-- >   bob <- C.index 1 people
+-- >   bob & C.encodeField #id 456
+-- >   bob & C.encodeField #name (T.pack "Bob")
+-- >   bob & C.encodeField #email (T.pack "bob@example.com")
 -- >
--- >     phones <- bob & C.newField #phones 2
--- >     homePhone <- phones & C.index 0
--- >     homePhone & C.encodeField #number (T.pack "555-4567")
--- >     homePhone & C.encodeField #type_ Person'PhoneNumber'Type'home
--- >     workPhone <- phones & C.index 1
--- >     workPhone & C.encodeField #number (T.pack "555-7654")
--- >     workPhone & C.encodeField #type_ Person'PhoneNumber'Type'work
--- >     employment <- bob & C.readField #employment
--- >     employment & C.encodeVariant #selfEmployed () -- Note the (), since selfEmploy is Void.
--- >
--- >     pure msg
+-- >   phones <- bob & C.newField #phones 2
+-- >   homePhone <- phones & C.index 0
+-- >   homePhone & C.encodeField #number (T.pack "555-4567")
+-- >   homePhone & C.encodeField #type_ Person'PhoneNumber'Type'home
+-- >   workPhone <- phones & C.index 1
+-- >   workPhone & C.encodeField #number (T.pack "555-7654")
+-- >   workPhone & C.encodeField #type_ Person'PhoneNumber'Type'work
+-- >   employment <- bob & C.readField #employment
+-- >   employment & C.encodeVariant #selfEmployed () -- Note the (), since selfEmploy is Void.
+-- >   pure msg
 
 -- $rpc
 --
@@ -540,6 +544,7 @@ import System.IO (stdout)
 --
 -- Here is an an echo (networking) server using this interface:
 --
+-- >
 -- > {-# LANGUAGE MultiParamTypeClasses #-}
 -- > {-# LANGUAGE OverloadedStrings #-}
 -- > {-# LANGUAGE TypeApplications #-}
@@ -585,7 +590,7 @@ import System.IO (stdout)
 -- 'waitPipeline' blocks until the result is available.
 --
 -- Here is an an echo client using this interface:
---
+
 -- > {-# LANGUAGE OverloadedLabels #-}
 -- > {-# LANGUAGE OverloadedStrings #-}
 -- >
